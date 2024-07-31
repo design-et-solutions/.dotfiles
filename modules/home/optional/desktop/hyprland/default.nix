1c84727f0ead6a7e69cc70713a4d15e54b1a493c
YvesCousteau <hugohenrotte@hotmail.com>
2024-08-01 00:54:27 +0200

    fix

--- modules/home/optional/desktop/hyprland/default.nix
+++ modules/home/optional/desktop/hyprland/default.nix
@@ -2,27 +2,5 @@
 {
   wayland.windowManager.hyprland = {
     enable = true;
-    # extraConfig = ''
-    #   # Hyprland configuration
-    #   monitor=,preferred,auto,auto
-      
-    #   # Start some programs
-    #   exec-once = waybar & hyprpaper & firefox
-      
-    #   # Set programs to launch with a keybind
-    #   bind = SUPER, Return, exec, alacritty
-    #   bind = SUPER, Q, killactive,
-    #   bind = SUPER, M, exit,
-    #   bind = SUPER, E, exec, dolphin
-    #   bind = SUPER, V, togglefloating,
-    #   bind = SUPER, R, exec, wofi --show drun
-    #   bind = SUPER, P, pseudo,
-    # '';
   };
-
-  home.packages = with pkgs; [
-    # waybar
-    # hyprpaper
-    # wofi
-  ];
 }
