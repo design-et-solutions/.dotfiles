3b98586b69b32a1df0490358cd3424a22b3aefe8
YvesCousteau <hugohenrotte@hotmail.com>
2024-08-01 00:18:30 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -32,46 +32,5 @@
         ];
       };
     };
-  #   # NixOS configuration entrypoint
-  #   # Available through 'nixos-rebuild --flake .#hostname'
-  #   nixosConfigurations = {
-  #       desktop-home = nixpkgs.lib.nixosSystem {
-  #           specialArgs = {inherit inputs outputs;};
-  #           modules = [./hosts/desktop/home];
-  #       };
-  #       laptop-work = nixpkgs.lib.nixosSystem {
-  #           specialArgs = {inherit inputs outputs;};
-  #           modules = [./hosts/laptop/work];
-  #       };
-  #       server-rpi5-home = nixpkgs.lib.nixosSystem {
-  #           specialArgs = {inherit inputs outputs;};
-  #           modules = [./hosts/server/rpi5/home];
-  #       };
-  #   };
-
-  #   # Standalone home-manager configuration entrypoint
-  #   # Available through 'home-manager --flake .#username@hostname'
-  #   homeConfigurations = {
-  #     desktop-home = home-manager.lib.homeManagerConfiguration {
-  #       system = "x86_64-linux";
-  #       homeDirectory = "/home/me";
-  #       username = "me";
-  #       configuration = import ./hosts/desktop/home;
-  #     };
-
-  #     laptop-work = home-manager.lib.homeManagerConfiguration {
-  #       system = "x86_64-linux";
-  #       homeDirectory = "/home/me";
-  #       username = "me";
-  #       configuration = import ./hosts/laptop/work;
-  #     };
-
-  #     server-rpi5 = home-manager.lib.homeManagerConfiguration {
-  #       system = "aarch64-linux";
-  #       homeDirectory = "/home/root";
-  #       username = "root";
-  #       configuration = import ./hosts/server/rpi5/home;
-  #     };
-  #   };
-  # };
+  };
 }
