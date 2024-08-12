a558056bcad2eec9c35d700ea05e020b2ab1e5bd
YvesCousteau <hugohenrotte@hotmail.com>
2024-08-12 13:34:21 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.groups.me = {};
+            users.groups.me = {};
             home-manager.users.me = import ./home/users/me;
           }
         ];
