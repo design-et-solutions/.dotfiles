3ae45f3ebef85c5bc478d2f295ceff7847af6d68
YvesCousteau <hugohenrotte@hotmail.com>
2024-08-13 16:20:19 +0200

    fix

--- home/optional/pkgs/zerox-proto/default.nix
+++ home/optional/pkgs/zerox-proto/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   version = "1.0";
 
   src = fetchzip {
-    url = "https://github.com/0xType/0xProto/releases/download/v1.003/0xProto.zip";
+    url = "https://github.com/0xType/0xProto/releases/download/v2.100/0xProto.zip";
     sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
   };
 
