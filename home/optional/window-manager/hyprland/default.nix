db56e90c78bbe6d060b5df96cea5e6cea40e831e
YvesCousteau <hugohenrotte@hotmail.com>
2024-08-13 17:32:35 +0200

    fix

--- home/optional/window-manager/hyprland/default.nix
+++ home/optional/window-manager/hyprland/default.nix
@@ -4,6 +4,7 @@
     "hypr/hyprland.conf".source = ../../../custom/hypr/hyprland.conf;
     "hypr/windowrule.conf".source = ../../../custom/hypr/windowrule.conf;
     "hypr/keybinds.conf".source = ../../../custom/hypr/keybinds.conf;
+    "hypr/env.conf".source = ../../../custom/hypr/env.conf;
 
     "wezterm/wezterm.lua".source = ../../../custom/wezterm/wezterm.lua;
     "wezterm/lua".source = ../../../custom/wezterm/lua;
