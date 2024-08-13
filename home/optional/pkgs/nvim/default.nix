68112b44dad7e6e0dce5516d32ff47f3f91c4949
YvesCousteau <hugohenrotte@hotmail.com>
2024-08-13 11:59:52 +0200

    fix

--- home/optional/pkgs/nvim/default.nix
+++ home/optional/pkgs/nvim/default.nix
@@ -2,5 +2,5 @@
 {
   programs.neovim.enable = true;
 
-  xdg.configFile."nvim/init.lua".text = builtins.readFile ../../custom/nvim/init.lua;
+  xdg.configFile."nvim/init.lua".text = builtins.readFile ../../../custom/nvim/init.lua;
 }
