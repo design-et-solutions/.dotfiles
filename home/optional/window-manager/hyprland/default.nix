5017d8871d11581101fbe42dcc7f55400b56c714
YvesCousteau <hugohenrotte@hotmail.com>
2024-08-13 12:29:28 +0200

    fix

--- home/optional/window-manager/hyprland/default.nix
+++ home/optional/window-manager/hyprland/default.nix
@@ -1,4 +1,8 @@
 { ... }:
 {
-    xdg.configFile."hypr/hyprland.conf".text = builtins.readFile ../../../custom/hypr/hyprland.conf;
+  xdg.configFile = {
+    "hypr/hyprland.conf".source = ../../../custom/hypr/hyprland.conf;
+    "hypr/windowrule.conf".source = ../../../custom/hypr/windowrule.conf;
+    "hypr/keybinds.conf".source = ../../../custom/hypr/keybinds.conf;
+  };
 }
