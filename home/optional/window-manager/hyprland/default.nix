ff67b15c48fbd603e7d1a63b75a1a44530492ba2
YvesCousteau <hugohenrotte@hotmail.com>
2024-08-13 11:58:14 +0200

    fix

--- home/optional/window-manager/hyprland/default.nix
+++ home/optional/window-manager/hyprland/default.nix
@@ -1,4 +1,5 @@
 { ... }:
 {
     xdg.configFile."hypr/hyprland.conf".text = builtins.readFile ../../../custom/hypr/hyprland.conf;
+    xdg.configFile."nvim/init.lua".text = builtins.readFile ../../../custom/nvim/init.lua;
 }
