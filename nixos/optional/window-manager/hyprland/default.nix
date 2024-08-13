53d24a364e356286da12595385f76d9aab8634dd
YvesCousteau <hugohenrotte@hotmail.com>
2024-08-13 11:20:40 +0200

    try to customize hyprland

--- nixos/optional/window-manager/hyprland/default.nix
+++ nixos/optional/window-manager/hyprland/default.nix
@@ -3,6 +3,7 @@
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
+    extraConfig = builtins.readFile ../../../../hyprland/hyprland.conf;
   };
 
   programs.thunar.enable = true;
