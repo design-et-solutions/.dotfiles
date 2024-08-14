8f4ac9b94fa2c12224ed933945f524cd1643472d
YvesCousteau <hugohenrotte@hotmail.com>
2024-08-14 12:21:21 +0200

    fix

--- home/optional/window-manager/hyprland/default.nix
+++ home/optional/window-manager/hyprland/default.nix
@@ -4,6 +4,7 @@
     "hypr/hyprland.conf".source = ../../../custom/hypr/hyprland.conf;
     "hypr/windowrule.conf".source = ../../../custom/hypr/windowrule.conf;
     "hypr/keybinds.conf".source = ../../../custom/hypr/keybinds.conf;
+    "hypr/scripts".source = ../../../custom/hypr/scripts;
 
     "kitty/kitty.conf".source = ../../../custom/kitty/kitty.conf;
 
