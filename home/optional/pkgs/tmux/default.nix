db79649ff76ac9ded30a19c8cf65b018f2bb1c1b
username <email@domain.com>
2024-08-19 15:52:13 +0000

    tmux done

--- home/optional/pkgs/tmux/default.nix
+++ home/optional/pkgs/tmux/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   programs.tmux = {
     enable = true;
-    shell = "\${pkgs.fish}/bin/fish";
+    shell = "${pkgs.fish}/bin/fish";
     mouse = true;
     plugins = with pkgs; [
       tmuxPlugins.cpu
@@ -21,12 +21,32 @@
     keyMode = "vi";
     baseIndex = 1;
     extraConfig = ''
-# automatically move window after deleting window
-set -g renumber-windows on
+      # automatically move window after deleting window
+      set -g renumber-windows on
+
+      # split panes using | and -
+      bind _ split-window -h
+      bind - split-window -v
+      unbind '"'
+      unbind %
+
+      # switch panes using Alt-arrow without prefix
+      bind -n M-Left select-pane -L
+      bind -n M-Right select-pane -R
+      bind -n M-Up select-pane -U
+      bind -n M-Down select-pane -D
+
+      # Keeping Current Path 
+      bind c new-window -c "#{pane_current_path}"
+
+      # Toggle between the current and previous window
+      bind a last-window
+
+      # Resizing
+      bind -r C-j resize-pane -D 15
+      bind -r C-k resize-pane -U 15
+      bind -r C-h resize-pane -L 15
+      bind -r C-l resize-pane -R 15
     '';
   };
-
-  home.file = {
-    ".tmux.conf".source = ./tmux.conf;
-  }; 
 }
