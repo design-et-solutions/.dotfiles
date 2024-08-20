8a6b68e69fd86e874e3a7bec9d7af2bd7da504e2
username <email@domain.com>
2024-08-20 15:26:10 +0000

    add gruvbox on kitty

--- home/optional/pkgs/kitty/default.nix
+++ home/optional/pkgs/kitty/default.nix
@@ -7,21 +7,12 @@
     };
     settings = {
       shell = "tmux";
-      foreground = "#e0def4";
-      cursor = "#59546d";
       window_padding_width = 4;
     };
     keybindings = {
       "ctrl+shift+c" = "copy_to_clipboard";
       "ctrl+shift+v" = "paste_from_clipboard";
     };
-    extraConfig = ''
-      italic_font auto
-      bold_font auto
-      bold_italic_font auto
-
-      background #232136
-      background_opacity 0.9
-    '';
+    extraConfig = builtins.readFile ./config.conf;
   };
 }
