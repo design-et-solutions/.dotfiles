620a7db1d7eb870ab9b1e88258caa4b168235d6a
me <email@domain.com>
2024-08-26 19:25:36 +0200

    python packages used for work

--- nixos/optional/pkgs/python/default.nix
+++ nixos/optional/pkgs/python/default.nix
@@ -2,6 +2,11 @@
 {
   environment.systemPackages = with pkgs; [
     (python3.withPackages(ps: with ps; [
+      flask
+      flask-socketio
+      eventlet
+      colorama
+      pynput
     ]))
   ];
 }
