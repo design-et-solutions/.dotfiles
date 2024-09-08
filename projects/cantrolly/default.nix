3fd335209aa0e560e60702ba2ba5097cbdfb3616
Your Name <you@example.com>
2024-09-08 16:44:46 +0200

    fix

--- projects/cantrolly/default.nix
+++ projects/cantrolly/default.nix
@@ -3,7 +3,7 @@
   environment = {
     etc = {
       "cantrolly/.env".source = ./.env;
-      "cantrolly/bin".source = ../../../cantrolly;
+      "cantrolly/cantrolly".source = ../../../cantrolly;
     };
   }; 
 
