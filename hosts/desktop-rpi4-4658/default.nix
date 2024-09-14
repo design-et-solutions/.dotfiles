1450a0c98330935481e06d62d502d36624dad25b
Your Name <you@example.com>
2024-09-14 03:35:27 +0200

    fix

--- hosts/desktop-rpi4-4658/default.nix
+++ hosts/desktop-rpi4-4658/default.nix
@@ -13,7 +13,7 @@
     # Increase if you experience kernel panics
     kernelParams = [
       "cma=256M" 
-      "dtoverlay=vc4-fkms-v3d"
+      "dtoverlay=vc4-kms-v3d"
       "snd_bcm2835.enable_headphones=1"
       "snd_bcm2835.enable_hdmi=1"
     ];
