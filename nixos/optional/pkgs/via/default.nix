c91f951f500ea825894b783c63b41bfbf010c7fe
me <email@domain.com>
2024-10-07 16:26:29 +0200

    update via and add keybord config

--- nixos/optional/pkgs/via/default.nix
+++ nixos/optional/pkgs/via/default.nix
@@ -1,5 +1,8 @@
 { pkgs, lib, ... }:{
   environment.systemPackages = with pkgs; [
-    via
+    vial
   ];
+  services.udev.extraRules = ''
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{serial}=="*vial:f64c2b3c*", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
+  '';
 }
