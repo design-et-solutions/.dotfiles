015b916896940846e3ed6927024eac87bc93bb49
Your Name <you@example.com>
2024-11-16 16:09:32 +0100

    add clippy alias

--- nixos/core/pkgs/rust/default.nix
+++ nixos/core/pkgs/rust/default.nix
@@ -8,10 +8,12 @@
     clippy
     rustup
     pkg-config
-    openssl
+    openssl   
   ];
 
   environment.variables = {
     PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+    LD_LIBRARY_PATH = "${pkgs.openssl}/lib:${pkgs.openssl.dev}/lib:$LD_LIBRARY_PATH";
   };
 }
+
--- nixos/core/shell/fish/default.nix
+++ nixos/core/shell/fish/default.nix
@@ -6,8 +6,27 @@
       ll = "ls -l";
       la = "ls -a";
       lg = "lazygit"; 
-      clippy-hard = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::all -W clippy::pedantic -W clippy::nursery -W clippy::cargo"; 
-      # to install `rustup run nightly cargo install cargo-udeps`
+      clippy-full = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::all -W clippy::pedantic -W clippy::nursery -W clippy::cargo -W clippy::perf -W clippy::complexity -W clippy::suspicious -W clippy::style -W clippy::correctness"; 
+      # all lints that are on by default (correctness, suspicious, style, complexity, perf)
+      clippy-all = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::all"; 
+      # lints for the cargo manifest
+      clippy-cargo = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::cargo"; 
+      # new lints that are still under development
+      clippy-nursery = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::nursery"; 
+      # lints which are rather strict or have occasional false positives
+      clippy-pedantic = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::pedantic"; 
+      # code that can be written to run faster
+      clippy-perf = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::perf"; 
+      # lints which prevent the use of language and library features1
+      clippy-restriction = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::restriction"; 
+      # code that does something simple but in a complex way
+      clippy-complexity = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::complexity"; 
+      # code that should be written in a more idiomatic way
+      clippy-style = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::style"; 
+      # code that is most likely wrong or useless
+      clippy-suspicious = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::suspicious"; 
+      # code that is outright wrong or useless
+      clippy-correctness = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::correctness"; 
       cargo-check = "rustup run nightly cargo udeps --all-targets  --all-features"; 
     };
     interactiveShellInit = ''
