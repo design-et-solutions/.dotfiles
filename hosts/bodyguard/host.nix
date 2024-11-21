204998c68c18238d2da88e6185bac06f5e6934b6
Your Name <you@example.com>
2024-11-21 11:28:08 +0100

    fix bodyguard

--- hosts/bodyguard/host.nix
+++ hosts/bodyguard/host.nix
@@ -2,7 +2,7 @@
 { mkNixosConfiguration, nixos-hardware, ... }:
 
 mkNixosConfiguration {
-  system = "arrm64-linux";
+  system = "aarch64-linux";
   host = ./.;
   users = [ "bodyguard" ];
   setup = {};
