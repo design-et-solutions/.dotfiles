99bfd35b9938b53279a18507625eb1b5160ff838
Your Name <you@example.com>
2024-11-21 11:31:55 +0100

    fix

--- hosts/bodyguard/default.nix
+++ hosts/bodyguard/default.nix
@@ -18,51 +18,51 @@
     # Use as a Secure Gateway
     wireguard = {
       enable = true;
-      interfaces = {
+      # interfaces = {
         # Generate Keys for WireGuard:
         # $wg genkey | tee /etc/wireguard/private.key | wg pubkey > /etc/wireguard/public.key
-        wg0 = {
-          # listens on for incoming connections
-          listenPort = 51820;
-          # IP address in the VPN's private network
-          addresses = [ "10.0.0.1/24" ];
-          privateKeyFile = "/etc/wireguard/private.key";
-          # Defines the devices (clients) allowed to connect to the server
-          peers = [
-            # Each peer is identified by its publicKey and the allowedIPs it can use
-            {
-              # laptop
-              publicKey = "peer-public-key";
-              allowedIPs = [ "10.0.0.2/32" ];
-            }
-            {
-              # desktop
-              publicKey = "peer-public-key";
-              allowedIPs = [ "10.0.0.3/32" ];
-            }
-            {
-              # tv
-              publicKey = "peer-public-key";
-              allowedIPs = [ "10.0.0.4/32" ];
-            }
-            {
-              # phone1
-              publicKey = "peer-public-key";
-              allowedIPs = [ "10.0.0.4/32" ];
-            }
-            {
-              # phone2
-              publicKey = "peer-public-key";
-              allowedIPs = [ "10.0.0.5/32" ];
-            }
-            {
-              # print
-              publicKey = "peer-public-key";
-              allowedIPs = [ "10.0.0.6/32" ];
-            }
-          ];
-        };
-      };
+        # wg0 = {
+        #   # listens on for incoming connections
+        #   listenPort = 51820;
+        #   # IP address in the VPN's private network
+        #   addresses = [ "10.0.0.1/24" ];
+        #   privateKeyFile = "/etc/wireguard/private.key";
+        #   # Defines the devices (clients) allowed to connect to the server
+        #   peers = [
+        #     # Each peer is identified by its publicKey and the allowedIPs it can use
+        #     {
+        #       # laptop
+        #       publicKey = "peer-public-key";
+        #       allowedIPs = [ "10.0.0.2/32" ];
+        #     }
+        #     {
+        #       # desktop
+        #       publicKey = "peer-public-key";
+        #       allowedIPs = [ "10.0.0.3/32" ];
+        #     }
+        #     {
+        #       # tv
+        #       publicKey = "peer-public-key";
+        #       allowedIPs = [ "10.0.0.4/32" ];
+        #     }
+        #     {
+        #       # phone1
+        #       publicKey = "peer-public-key";
+        #       allowedIPs = [ "10.0.0.4/32" ];
+        #     }
+        #     {
+        #       # phone2
+        #       publicKey = "peer-public-key";
+        #       allowedIPs = [ "10.0.0.5/32" ];
+        #     }
+        #     {
+        #       # print
+        #       publicKey = "peer-public-key";
+        #       allowedIPs = [ "10.0.0.6/32" ];
+        #     }
+        #   ];
+        # };
+      # };
     };
     # allow VPN Port
     firewall = {
