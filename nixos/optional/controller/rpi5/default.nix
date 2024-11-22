eb297c004a47a58fcd6060cabfef78e9841082fb
Your Name <you@example.com>
2024-11-22 23:58:29 +0100

    fix

--- nixos/optional/controller/rpi5/default.nix
+++ nixos/optional/controller/rpi5/default.nix
@@ -3,6 +3,12 @@
     lm_sensors 
   ];
 
+  hardware = {
+    firmware = with pkgs; [
+      linux-firmware
+    ];
+  };
+
   boot = {
     loader.grub.device = "nodev";
     kernelPackages = inputs.nix-rpi5.legacyPackages.aarch64-linux.linuxPackages_rpi5;
