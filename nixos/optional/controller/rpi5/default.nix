46cd8b8626566b30a9196f52f23e6020de3cdcdf
Your Name <you@example.com>
2024-11-22 23:43:09 +0100

    fix

--- nixos/optional/controller/rpi5/default.nix
+++ nixos/optional/controller/rpi5/default.nix
@@ -6,5 +6,8 @@
   boot = {
     loader.grub.device = "nodev";
     kernelPackages = inputs.nix-rpi5.legacyPackages.aarch64-linux.linuxPackages_rpi5;
+    kernelModules = [ 
+      "brcmfmac" 
+    ];
   };
 }
