fe4b548ac82446ffe65d9efdc9d0f3e872d9f159
Your Name <you@example.com>
2024-11-22 18:04:22 +0100

    fix

--- nixos/optional/controller/rpi5/default.nix
+++ nixos/optional/controller/rpi5/default.nix
@@ -6,14 +6,5 @@
   boot = {
     loader.grub.device = "nodev";
     kernelPackages = inputs.nix-rpi5.legacyPackages.aarch64-linux.linuxPackages_rpi5;
-    # kernelModules = [ 
-    #   "bcm2835_thermal" 
-    #   "gpio-fan" "i2c-dev" 
-    # ];
-    # kernelParams = [
-    #   "dtoverlay=rpi-poe"
-    #   "dtparam=i2c_arm=on"
-    #   "dtoverlay=gpio-fan,gpiopin=14,temp=55000"
-    # ];
   };
 }
