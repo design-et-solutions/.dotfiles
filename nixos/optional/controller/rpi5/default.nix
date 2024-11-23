2ef981a8ed3b03f2ae31540d828eb861376ff1c3
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2024-11-23 05:24:09 +0100

    fix

--- nixos/optional/controller/rpi5/default.nix
+++ nixos/optional/controller/rpi5/default.nix
@@ -15,7 +15,7 @@
     description = "Install custom firmware into /lib/firmware";
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      ExecPre = "${pkgs.bash}/bin/bash -c 'mkdir -p /lib/firmware/brcm/'"
+      ExecPre = "${pkgs.bash}/bin/bash -c 'mkdir -p /lib/firmware/brcm/'";
       ExecStart = "${pkgs.bash}/bin/bash -c 'cp /nix/store/*-firmware/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.*.zst $out/lib/firmware/brcm/ ; ${pkgs.zstd}/bin/zstd -d /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.clm_blob.zst -o /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.clm_blob ; ${pkgs.zstd}/bin/zstd -d /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.bin.zst -o /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.bin ; ${pkgs.zstd}/bin/zstd -d /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.txt.zst -o /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.txt ; rm /lib/frimware/brcm/*.zst'";
       ExecStartPost = "${pkgs.coreutils}/bin/ls -l /lib/firmware/brcm";
       Type = "oneshot";
