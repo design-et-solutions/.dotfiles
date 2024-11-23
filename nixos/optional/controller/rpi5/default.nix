048f1bb562aa41f05bce81c692294103ba4d6a64
Your Name <you@example.com>
2024-11-23 01:38:06 +0100

    fix

--- nixos/optional/controller/rpi5/default.nix
+++ nixos/optional/controller/rpi5/default.nix
@@ -12,9 +12,9 @@
 
   hardware.firmware = [
     (pkgs.runCommand "rpi-firmware" { buildInputs = [ pkgs.wget ]; } ''
-      mkdir -p /lib/firmware/brcm
-      wget https://github.com/RPi-Distro/firmware-nonfree/blob/bookworm/debian/config/brcm80211/brcm/brcmfmac43455-sdio.bin -P /lib/firmware/brcm/brcmfmac43455-sdio.bin
-      wget https://github.com/RPi-Distro/firmware-nonfree/blob/bookworm/debian/config/brcm80211/brcm/brcmfmac43455-sdio.txt -P /lib/firmware/brcm/brcmfmac43455-sdio.txt
+      mkdir -p $out/lib/firmware/brcm
+      wget https://github.com/RPi-Distro/firmware-nonfree/blob/bookworm/debian/config/brcm80211/brcm/brcmfmac43455-sdio.bin -P $out/lib/firmware/brcm/brcmfmac43455-sdio.bin
+      wget https://github.com/RPi-Distro/firmware-nonfree/blob/bookworm/debian/config/brcm80211/brcm/brcmfmac43455-sdio.txt -P $out/lib/firmware/brcm/brcmfmac43455-sdio.txt
     '')
   ];
 }
