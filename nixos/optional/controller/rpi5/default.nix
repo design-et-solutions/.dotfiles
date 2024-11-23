61530581bc5c28673a4bd322deff024d4c5d28cd
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2024-11-23 04:31:59 +0100

    fix

--- nixos/optional/controller/rpi5/default.nix
+++ nixos/optional/controller/rpi5/default.nix
@@ -23,4 +23,16 @@
       cp ${pkgs.linux-firmware}/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.txt $out/lib/firmware/brcm/
     '')
   ];
+
+  systemd.services.install-custom-firmware = {
+    description = "Install custom firmware into /lib/firmware";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.coreutils}/bin/cp -r /nix/store/*custom-brcmfmac-firmware*/lib/firmware/brcm /lib/firmware/";
+      ExecStartPost = "${pkgs.coreutils}/bin/ls -l /lib/firmware/brcm";
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+  };
 }
