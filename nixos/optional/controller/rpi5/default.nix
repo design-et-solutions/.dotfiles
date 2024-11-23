be23f220d64403838f9722abffacc1e4ea64db1f
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2024-11-23 04:02:38 +0100

    fix

--- nixos/optional/controller/rpi5/default.nix
+++ nixos/optional/controller/rpi5/default.nix
@@ -8,8 +8,25 @@
     loader.grub.device = "nodev";
     kernelPackages = inputs.nix-rpi5.legacyPackages.aarch64-linux.linuxPackages_rpi5;
     loader.efi.canTouchEfiVariables = lib.mkForce false;
-    initrd.availableKernelModules = [ "brcmfmac" ];
   };
 
-  hardware.firmware = [ pkgs.linux-firmware ];
+  hardware.firmware = [
+    pkgs.linux-firmware
+    (pkgs.runCommand "custom-brcmfmac-firmware" {
+      buildInputs = [ pkgs.zstd ];
+    } ''
+      mkdir -p $out/lib/firmware/brcm
+
+      # Copy firmware files
+      cp ${pkgs.linux-firmware}/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.* $out/lib/firmware/brcm/
+
+      # Decompress firmware files
+      zstd -d $out/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.bin.zst -o $out/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.bin
+      zstd -d $out/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.clm_blob.zst -o $out/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.clm_blob
+      zstd -d $out/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.txt.zst -o $out/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.txt
+
+      # Remove compressed files
+      rm $out/lib/firmware/brcm/*.zst
+    '')
+  ];
 }
