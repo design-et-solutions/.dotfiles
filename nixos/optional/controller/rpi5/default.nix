c8103046203fb3a35d7c3b9b8735cb9f7a9b68a1
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2024-11-23 05:10:32 +0100

    test

--- nixos/optional/controller/rpi5/default.nix
+++ nixos/optional/controller/rpi5/default.nix
@@ -10,32 +10,41 @@
     loader.efi.canTouchEfiVariables = lib.mkForce false;
   };
 
-  hardware.firmware = [
-    pkgs.linux-firmware
-    (pkgs.runCommand "custom-brcmfmac-firmware" {
-      buildInputs = [ pkgs.zstd pkgs.coreutils ];
-    } ''
-      mkdir -p $out/lib/firmware/brcm
-
-      # cp /nix/store/*-firmware/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.txt.zst $out/lib/firmware/brcm/
-
-      # sudo zstd -d /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.clm_blob.zst -o /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.clm_blob
-      # sudo zstd -d /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.txt.zst -o /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.txt
-      # sudo zstd -d /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.bin.zst -o /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.bin
-
-      cp ${pkgs.linux-firmware}/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.txt $out/lib/firmware/brcm/
-      cp ${pkgs.linux-firmware}/lib/firmware/brcm/brcmfmac43455-sdio.clm_blob $out/lib/firmware/brcm/
-      cp ${pkgs.linux-firmware}/lib/firmware/brcm/brcmfmac43455-sdio.bin $out/lib/firmware/brcm/
-
-      # cp /nix/store/*-firmware/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.*.zst $out/lib/firmware/brcm/
-    '')
-  ];
+  # hardware.firmware = [
+  #   pkgs.linux-firmware
+  #   (pkgs.runCommand "custom-brcmfmac-firmware" {
+  #     buildInputs = [ pkgs.${pkgs.zst}/bin/zstd pkgs.coreutils ];
+  #   } ''
+  #     mkdir -p $out/lib/firmware/brcm
+  #
+  #     # cp /nix/store/*-firmware/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.txt.zst $out/lib/firmware/brcm/
+  #
+  #     # sudo ${pkgs.zst}/bin/zstd -d /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.clm_blob.zst -o /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.clm_blob
+  #     # sudo ${pkgs.zst}/bin/zstd -d /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.txt.zst -o /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.txt
+  #     # sudo ${pkgs.zst}/bin/zstd -d /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.bin.zst -o /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.bin
+  #
+  #     cp ${pkgs.linux-firmware}/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.txt $out/lib/firmware/brcm/
+  #     cp ${pkgs.linux-firmware}/lib/firmware/brcm/brcmfmac43455-sdio.clm_blob $out/lib/firmware/brcm/
+  #     cp ${pkgs.linux-firmware}/lib/firmware/brcm/brcmfmac43455-sdio.bin $out/lib/firmware/brcm/
+  #   '')
+  # ];
+  #
+  # systemd.services.install-custom-firmware = {
+  #   description = "Install custom firmware into /lib/firmware";
+  #   wantedBy = [ "multi-user.target" ];
+  #   serviceConfig = {
+  #     ExecStart = "${pkgs.bash}/bin/bash -c 'cp -r $(readlink -f /nix/store/*custom-brcmfmac-firmware*/lib/firmware/brcm) /lib/firmware/'";
+  #     ExecStartPost = "${pkgs.coreutils}/bin/ls -l /lib/firmware/brcm";
+  #     Type = "oneshot";
+  #     RemainAfterExit = true;
+  #   };
+  # };
 
   systemd.services.install-custom-firmware = {
     description = "Install custom firmware into /lib/firmware";
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      ExecStart = "${pkgs.bash}/bin/bash -c 'cp -r $(readlink -f /nix/store/*custom-brcmfmac-firmware*/lib/firmware/brcm) /lib/firmware/'";
+      ExecStart = "${pkgs.bash}/bin/bash -c 'cp /nix/store/*-firmware/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.*.zst $out/lib/firmware/brcm/ && ${pkgs.zst}/bin/zstd -d /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.clm_blob.zst -o /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.clm_blob && zstd -d /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.bin.zst -o /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.bin && zstd -d /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.txt.zst -o /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.txt'";
       ExecStartPost = "${pkgs.coreutils}/bin/ls -l /lib/firmware/brcm";
       Type = "oneshot";
       RemainAfterExit = true;
