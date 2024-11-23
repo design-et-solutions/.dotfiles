b7ae3b807b307d3c97d3ab96829c2ad5c2827958
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2024-11-23 04:24:58 +0100

    test

--- nixos/optional/controller/rpi5/default.nix
+++ nixos/optional/controller/rpi5/default.nix
@@ -18,10 +18,13 @@
       mkdir -p $out/lib/firmware/brcm
 
       # cp /nix/store/*-firmware/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.txt.zst $out/lib/firmware/brcm/
+      # sudo zstd -d /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.clm_blob.zst -o /lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.clm_blob
 
-      ls /nix/store/*-firmware/lib/firmware/brcm/
+      # ls /nix/store/*-firmware/lib/firmware/brcm/
 
-      cp /nix/store/*-firmware/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.txt.zst $out/lib/firmware/brcm/ || echo "Failed to copy firmware"
+      # cp /nix/store/*-firmware/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.txt.zst $out/lib/firmware/brcm/ || echo "Failed to copy firmware"
+
+      cp ${pkgs.linux-firmware}/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,5-model-b.txt.zst $out/lib/firmware/brcm/
 
       # Remove compressed files
       rm $out/lib/firmware/brcm/*.zst
