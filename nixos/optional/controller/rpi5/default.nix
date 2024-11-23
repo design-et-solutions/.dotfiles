73535995cbf29cca2ca0db1b2e06287e462d3de7
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2024-11-23 03:01:35 +0100

    fix

--- nixos/optional/controller/rpi5/default.nix
+++ nixos/optional/controller/rpi5/default.nix
@@ -22,10 +22,21 @@ in
   };
 
   hardware.firmware = [
+    # (pkgs.runCommand "rpi-firmware" { } ''
+    #   mkdir -p $out/lib/firmware/brcm
+    #   cp ${brcm_firmware_bin} $out/lib/firmware/brcm/brcmfmac43455-sdio.bin
+    #   cp ${brcm_firmware_txt} $out/lib/firmware/brcm/brcmfmac43455-sdio.txt
+    # '')
     (pkgs.runCommand "rpi-firmware" { } ''
       mkdir -p $out/lib/firmware/brcm
-      cp ${brcm_firmware_bin} $out/lib/firmware/brcm/brcmfmac43455-sdio.bin
-      cp ${brcm_firmware_txt} $out/lib/firmware/brcm/brcmfmac43455-sdio.txt
+      cp ${pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/bookworm/debian/config/brcm80211/brcm/brcmfmac43455-sdio.bin";
+        sha256 = "0051d8bmhzna29wc9slp346ipvnckvr5lrhh4n7yfgvvxkbdgdg7";
+      }} $out/lib/firmware/brcm/brcmfmac43455-sdio.bin
+      cp ${pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/bookworm/debian/config/brcm80211/brcm/brcmfmac43455-sdio.txt";
+        sha256 = "0gcyrsirjx8j2481brps0ypxgjrsjhwlydrn569vdgbq3bl9nw6a";
+      }} $out/lib/firmware/brcm/brcmfmac43455-sdio.txt
     '')
   ];
 }
