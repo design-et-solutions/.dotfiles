033fa2f07d095494ffff2eb2b7cc0b0efd8773eb
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2024-11-23 03:06:49 +0100

    test

--- nixos/optional/controller/rpi5/default.nix
+++ nixos/optional/controller/rpi5/default.nix
@@ -1,15 +1,4 @@
 { pkgs, inputs, lib, ... }:
-let
-  brcm_firmware_bin = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/bookworm/debian/config/brcm80211/brcm/brcmfmac43455-sdio.bin";
-    sha256 = "0051d8bmhzna29wc9slp346ipvnckvr5lrhh4n7yfgvvxkbdgdg7";
-  };
-
-  brcm_firmware_txt = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/bookworm/debian/config/brcm80211/brcm/brcmfmac43455-sdio.txt";
-    sha256 = "0gcyrsirjx8j2481brps0ypxgjrsjhwlydrn569vdgbq3bl9nw6a";
-  };
-in
 {
   environment.systemPackages = with pkgs; [ 
     lm_sensors 
@@ -20,23 +9,4 @@ in
     kernelPackages = inputs.nix-rpi5.legacyPackages.aarch64-linux.linuxPackages_rpi5;
     loader.efi.canTouchEfiVariables = lib.mkForce false;
   };
-
-  hardware.firmware = [
-    # (pkgs.runCommand "rpi-firmware" { } ''
-    #   mkdir -p $out/lib/firmware/brcm
-    #   cp ${brcm_firmware_bin} $out/lib/firmware/brcm/brcmfmac43455-sdio.bin
-    #   cp ${brcm_firmware_txt} $out/lib/firmware/brcm/brcmfmac43455-sdio.txt
-    # '')
-    (pkgs.runCommand "rpi-firmware" { } ''
-      mkdir -p $out/lib/firmware/brcm
-      cp ${pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/bookworm/debian/config/brcm80211/brcm/brcmfmac43455-sdio.bin";
-        sha256 = "0051d8bmhzna29wc9slp346ipvnckvr5lrhh4n7yfgvvxkbdgdg7";
-      }} $out/lib/firmware/brcm/brcmfmac43455-sdio.bin
-      cp ${pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/bookworm/debian/config/brcm80211/brcm/brcmfmac43455-sdio.txt";
-        sha256 = "0gcyrsirjx8j2481brps0ypxgjrsjhwlydrn569vdgbq3bl9nw6a";
-      }} $out/lib/firmware/brcm/brcmfmac43455-sdio.txt
-    '')
-  ];
 }
