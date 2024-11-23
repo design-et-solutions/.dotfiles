e23ab5899dab5d74fa401b26c04c96109f88cbd6
Your Name <you@example.com>
2024-11-23 01:39:50 +0100

    fix

--- nixos/optional/controller/rpi5/default.nix
+++ nixos/optional/controller/rpi5/default.nix
@@ -1,4 +1,15 @@
 { pkgs, inputs, lib, ... }:
+let
+  brcm_firmware_bin = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/bookworm/debian/config/brcm80211/brcm/brcmfmac43455-sdio.bin";
+    sha256 = "<INSERT_SHA256_FOR_BIN>";
+  };
+
+  brcm_firmware_txt = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/RPi-Distro/firmware-nonfree/bookworm/debian/config/brcm80211/brcm/brcmfmac43455-sdio.txt";
+    sha256 = "<INSERT_SHA256_FOR_TXT>";
+  };
+in
 {
   environment.systemPackages = with pkgs; [ 
     lm_sensors 
@@ -11,10 +22,10 @@
   };
 
   hardware.firmware = [
-    (pkgs.runCommand "rpi-firmware" { buildInputs = [ pkgs.wget ]; } ''
+    (pkgs.runCommand "rpi-firmware" { } ''
       mkdir -p $out/lib/firmware/brcm
-      wget https://github.com/RPi-Distro/firmware-nonfree/blob/bookworm/debian/config/brcm80211/brcm/brcmfmac43455-sdio.bin -P $out/lib/firmware/brcm/brcmfmac43455-sdio.bin
-      wget https://github.com/RPi-Distro/firmware-nonfree/blob/bookworm/debian/config/brcm80211/brcm/brcmfmac43455-sdio.txt -P $out/lib/firmware/brcm/brcmfmac43455-sdio.txt
+      cp ${brcm_firmware_bin} $out/lib/firmware/brcm/brcmfmac43455-sdio.bin
+      cp ${brcm_firmware_txt} $out/lib/firmware/brcm/brcmfmac43455-sdio.txt
     '')
   ];
 }
