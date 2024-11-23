97ec37cf0bc9c25420108b79552b5884c7ccc0cf
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2024-11-23 03:15:46 +0100

    test

--- nixos/optional/controller/rpi5/default.nix
+++ nixos/optional/controller/rpi5/default.nix
@@ -9,4 +9,19 @@
     kernelPackages = inputs.nix-rpi5.legacyPackages.aarch64-linux.linuxPackages_rpi5;
     loader.efi.canTouchEfiVariables = lib.mkForce false;
   };
+
+  systemd.services.copy-firmware = {
+    description = "Decompress and copy firmware files for brcmfmac";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = ''
+        mkdir -p /lib/firmware/brcm
+        for firmware in /nix/store/*-firmware/lib/firmware/brcm/*.zst; do
+          zstd -d "$firmware" -o "/lib/firmware/brcm/$(basename "$firmware" .zst)"
+        done
+      '';
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+  };
 }
