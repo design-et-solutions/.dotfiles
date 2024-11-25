e3d0b3804cd738c2e74260684b82efc29e1b4052
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2024-11-25 17:30:13 +0100

    fix

--- nixos/optional/controller/rpi5/default.nix
+++ nixos/optional/controller/rpi5/default.nix
@@ -30,6 +30,8 @@
   environment.variables = {
     GST_PLUGIN_SYSTEM_PATH_1_0="${pkgs.gst_all_1.gstreamer.out}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-base}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-good}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-bad}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-ugly}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-vaapi}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-libav}/lib/gstreamer-1.0";
     PATH="$PATH:${pkgs.gst_all_1.gstreamer.dev}/bin";
+    LIBVA_DRIVER_NAME = "v3d";
+    LIBVA_DRIVERS_PATH = "/run/opengl-driver/lib/dri";
   };
 
   services.xserver.videoDrivers = [ "modesetting" ];
