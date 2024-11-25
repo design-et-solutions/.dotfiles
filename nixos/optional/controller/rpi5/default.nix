4d48e19e1902d26e2f658556fca77f454bf8b28a
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2024-11-25 13:36:02 +0100

    fix

--- nixos/optional/controller/rpi5/default.nix
+++ nixos/optional/controller/rpi5/default.nix
@@ -11,8 +11,12 @@
     loader.efi.canTouchEfiVariables = lib.mkForce false;
     kernelParams = [ 
       "cma=256M" 
-      "dtoverlay=vc4-kms-v3d-pi5"
+      "dtoverlay=vc4-kms-v3d"
+      # "dtoverlay=vc4-kms-v3d-pi5"
     ];
+     extraConfig = ''
+      gpu_mem=256                  # Allocate 256MB for GPU rendering
+    '';
   };
 
   systemd.services.update-brcmfmac43455 = {
