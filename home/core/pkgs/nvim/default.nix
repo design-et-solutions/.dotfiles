242f6b65c3b9ec4ca3b49a68223de724a8235c4d
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2024-11-27 10:50:31 +0100

    refac: add neovim github

--- home/core/pkgs/nvim/default.nix
+++ home/core/pkgs/nvim/default.nix
@@ -33,44 +33,64 @@
       imagemagick
       postgresql
     ];
+    # https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/editors/vim/plugins/vim-plugin-names
     plugins = with pkgs.vimPlugins; [
-      nvim-lspconfig
-      vim-nix
-      vim-lastplace
-      rust-vim
-      coc-nvim
-      coc-rust-analyzer
-      gruvbox-nvim
-      tokyonight-nvim
-      nvim-tree-lua
-      nvim-web-devicons
-      conform-nvim
-      comment-nvim
-      markdown-preview-nvim
-      bufferline-nvim
-      telescope-nvim
-      toggleterm-nvim
-      nvim-colorizer-lua
-      fugitive
-      which-key-nvim
-      rest-nvim
-      image-nvim
-      vim-dadbod
-      vim-dadbod-ui
-      markdown-preview-nvim
+      bufferline-nvim         # https://github.com/akinsho/bufferline.nvim
+      coc-nvim                # https://github.com/neoclide/coc.nvim
+      coc-lua                 # https://github.com/josa42/coc-lua
+      coc-rust-analyzer       # ...
+      coc-tailwindcss         # https://github.com/iamcco/coc-tailwindcss
+      coc-fzf                 # https://github.com/antoinemadec/coc-fzf
+      comment-nvim            # https://github.com/numtostr/comment.nvim
+      conform-nvim            # https://github.com/stevearc/conform.nvim
+      fugitive                # https://github.com/tpope/vim-fugitive (git)
+      gruvbox-nvim            # https://github.com/ellisonleao/gruvbox.nvim
+      image-nvim              # https://github.com/samodostal/image.nvim
+      markdown-nvim           # https://github.com/tadmccorkle/markdown.nvim
+      markdown-preview-nvim   # https://github.com/iamcco/markdown-preview.nvim
+      nvim-colorizer-lua      # https://github.com/nvchad/nvim-colorizer.lua
+      nvim-lspconfig          # https://github.com/neovim/nvim-lspconfig
+      nvim-tree-lua           # https://github.com/nvim-tree/nvim-tree.lua
+      nvim-web-devicons       # https://github.com/nvim-tree/nvim-web-devicons
+      rest-nvim               # https://github.com/rest-nvim/rest.nvim
+      rust-vim                # https://github.com/rust-lang/rust.vim
+      telescope-coc-nvim      # https://github.com/fannheyward/telescope-coc.nvim
+      telescope-nvim          # https://github.com/nvim-telescope/telescope.nvim
+      toggleterm-nvim         # https://github.com/akinsho/toggleterm.nvim
+      tokyonight-nvim         # https://github.com/folke/tokyonight.nvim
+      vim-dadbod              # https://github.com/tpope/vim-dadbod
+      vim-dadbod-completion   # https://github.com/kristijanhusak/vim-dadbod-completion
+      vim-dadbod-ui           # https://github.com/kristijanhusak/vim-dadbod-ui
+      vim-nix                 # https://github.com/LnL7/vim-nix
+      vim-lastplace           # https://github.com/ethanholz/nvim-lastplace
+      which-key-nvim          # https://github.com/liuchengxu/vim-which-key
       {
+        # https://github.com/nvim-treesitter/nvim-treesitter
         plugin = nvim-treesitter.withPlugins (
           plugins: with plugins; [
-            tree-sitter-nix
-            tree-sitter-lua
-            tree-sitter-rust
+            tree-sitter-bash
             tree-sitter-c
-            tree-sitter-vim
-            tree-sitter-markdown
-            tree-sitter-javascript
-            tree-sitter-typescript
-            tree-sitter-html
+            tree-sitter-cmake
             tree-sitter-css
+            tree-sitter-gitignore
+            tree-sitter-html          
+            tree-sitter-javascript    
+            tree-sitter-json   
+            tree-sitter-latex   
+            tree-sitter-lua  
+            tree-sitter-markdown      
+            tree-sitter-markdown_inline      
+            tree-sitter-mermaid      
+            tree-sitter-nix           
+            tree-sitter-proto           
+            tree-sitter-python           
+            tree-sitter-rust          
+            tree-sitter-sql          
+            tree-sitter-toml          
+            tree-sitter-typescript    
+            tree-sitter-vim           
+            tree-sitter-vimdoc           
+            tree-sitter-yaml           
           ]
         );
         type = "lua";
