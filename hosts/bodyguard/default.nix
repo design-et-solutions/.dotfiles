9a9168eddcd3b392c91506113084817a0dae966e
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2024-12-17 15:27:05 +0100

    add: blocky to bodyguard

--- hosts/bodyguard/default.nix
+++ hosts/bodyguard/default.nix
@@ -37,12 +37,32 @@
   };
 
   services = {
-    pihole = {
+    blocky = {
       enable = true;
-    #   extraConfig = ''
-    #     BLOCKING_ENABLED=true
-    #     BLOCKING_METHOD=IP-NODATA-AAAA
-    #   '';
+      settings = {
+        # Port for incoming DNS Queries.
+        ports.dns = 53; 
+        upstreams.groups.default = [
+          # Using Cloudflare's DNS over HTTPS server for resolving queries.
+          "https://one.one.one.one/dns-query" 
+        ];
+        # For initially solving DoH/DoT Requests when no system Resolver is available.
+        bootstrapDns = {
+          upstream = "https://one.one.one.one/dns-query";
+          ips = [ "1.1.1.1" "1.0.0.1" ];
+        };
+        #Enable Blocking of certain domains.
+        blocking = {
+          denylists = {
+            #Adblocking
+            ads = ["https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"];
+          };
+          #Configure what block categories are used
+          clientGroupsBlock = {
+            default = [ "ads" ];
+          };
+        };
+      };
     };
     # ntp.enable = true;
     # ntopng = {
