e8146676bfba5bc97794f12ff7d6e6f622169013
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-01-02 00:04:39 +0100

    add git crypt

--- nixos/core/security/default.nix
+++ nixos/core/security/default.nix
@@ -4,6 +4,20 @@
     ./audit.nix
   ];
 
-  # environment.systemPackages = with pkgs; [ vulnix ];
+  environment.systemPackages = with pkgs; [ 
+    vulnix 
+
+    # Init
+    # echo "pinentry-program $(which pinentry)" >> ~/.gnupg/gpg-agent.conf
+    # Use
+    # ❯ gpg --full-generate-key
+    # ❯ gpg --list-keys
+    # ❯ gpg --export-secret-keys --armor MNOP3456 > private-key-backup.asc
+    # ❯ gpg --export --armor MNOP3456 > public-key-backup.asc
+    # ❯ gpg --import private-key-backup.asc
+    # ❯ gpg --import public-key-backup.asc
+    gnupg
+    pinentry-curses
+  ];
 }
 
