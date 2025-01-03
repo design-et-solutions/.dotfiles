dbeabe73e7dd8a6d83291cae56c9624e8528e81e
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-01-03 14:13:56 +0100

    fix systemd-analyser security service

--- nixos/core/security/services.nix
+++ nixos/core/security/services.nix
@@ -1,48 +1,17 @@
 { pkgs, ... }: {
+  environment.etc."scripts/systemd-analyzer-security.sh" = {
+    source = builtins.toString ../../scripts/systemd-analayser-security.sh;
+    mode = "0755";
+  };
+
   systemd.services.systemd-security-check = {
     description = "Run systemd security check";
-    script = ''
-      export PATH=${pkgs.coreutils}/bin:${pkgs.gawk}/bin:${pkgs.systemd}/bin:${pkgs.libnotify}/bin:${pkgs.procps}/bin:${pkgs.util-linux}/bin:$PATH
-
-      result=$(echo $(systemd-analyze security | awk '/OK/ {ok++} {total++} END {print "OK: " ok " / Total: " total}'))
-      echo $result
-
-      
-      ok=$(echo "$result" | awk '{print $3}')
-      total=$(echo "$result" | awk '{print $5}')
-
-      if [ "$ok" -eq "$total" ]; then
-        urgency="normal"
-      else
-        urgency="critical"
-      fi
-
-      loginctl list-sessions --no-legend  | while IFS= read -r session; do
-        echo "Processing session: $session"
-
-        session_id=$(echo "$session" | awk '{print $1}')
-        user_id=$(echo "$session" | awk '{print $2}')
-        username=$(echo "$session" | awk '{print $3}')
-        session_type=$(loginctl show-session "$session_id" -p Type | cut -d= -f2)
-
-        if [[ "$session_type" == "wayland" || "$session_type" == "x11" ]]; then
-          echo "GUI session detected: $session_type"
-
-          export XDG_RUNTIME_DIR="/run/user/$user_id"
-          export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u "$user_id" -n)/environ | tr -d '\0' | sed 's/DBUS_SESSION_BUS_ADDRESS=//')
-
-          echo "XDG_RUNTIME_DIR: $XDG_RUNTIME_DIR"
-          echo "DBUS_SESSION_BUS_ADDRESS: $DBUS_SESSION_BUS_ADDRESS"
-
-          runuser -u $username -- notify-send --urgency=$urgency --icon info --category info "Systemd Security Check" "$result"
-
-        else
-          echo "Non-GUI session skipped: $session_type"
-        fi
-      done
-    '';
     serviceConfig = {
       Type = "oneshot";
+      ExecStart = "${pkgs.bash}/bin/bash -c '/etc/scripts/systemd-analyzer-security.sh'";
+      Environment = [
+        "PATH=${pkgs.bash}/bin:${pkgs.gnugrep}/bin:${pkgs.gnused}/bin:${pkgs.coreutils}/bin:${pkgs.gawk}/bin:${pkgs.systemd}/bin:${pkgs.libnotify}/bin:${pkgs.procps}/bin:${pkgs.util-linux}/bin:$PATH"
+      ];
     };
     wantedBy = [ "multi-user.target" ];
   };
