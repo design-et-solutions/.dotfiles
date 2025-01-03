11e43453e9f172a181a15ae1c0703154eb362658
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-01-03 15:01:24 +0100

    wip

--- nixos/scripts/close-luks-usb.sh
+++ nixos/scripts/close-luks-usb.sh
@@ -2,11 +2,11 @@
 
 if [ -e /dev/mapper/encrypted-key ]; then
   echo "Unmounting and closing LUKS-encrypted USB key..."
-  umount /media/usb-key || {
+  sudo umount /media/usb-key || {
     echo "Failed to unmount filesystem!" >&2
     exit 1
   }
-  cryptsetup close encrypted-key || {
+  sudo cryptsetup close encrypted-key || {
     echo "Failed to close LUKS device!" >&2
     exit 1
   }
