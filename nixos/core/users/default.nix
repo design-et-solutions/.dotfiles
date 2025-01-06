caa75a822aac71cd519fd650b527fa61f63bb257
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-01-06 11:27:08 +0100

    update: user service hardened to 6.3

--- nixos/core/users/default.nix
+++ nixos/core/users/default.nix
@@ -4,37 +4,20 @@
     NoNewPrivileges = true;
 
     ProtectSystem = "full";
-    # ProtectKernelModules = true;
-    # ProtectControlGroups = true;
-    # ProtectKernelTunables = true;
-    # ProtectKernelLogs = true;
-    # ProtectClock = true; 
+    ProtectClock = true; 
     ProtectHostname = true;
-    # PrivateDevices = true;
 
     PrivateTmp = true;
     PrivateNetwork = true;
-
-    # SystemCallFilter = [
-    #   "~@mount"      # Deny mounting operations
-    #   "~@raw-io"     # Deny raw I/O operations
-    #   "~@keyring"    # Deny kernel keyring operations
-    # ];
-
 
     MemoryDenyWriteExecute = true;
 
     RestrictAddressFamilies = [ "AF_UNIX" ];
     RestrictNamespaces = true;
 
-    # ReadOnlyPaths = [ "/etc" "/usr" ];
+    NoExecPaths = [ "/home" "/tmp" "/var/tmp" ];
 
-    # IPAddressDeny = "any";
-
-    # LockPersonality = true;
-
-    # DevicePolicy = "closed";
-
+    SystemCallArchitectures = "native";
 
     CapabilityBoundingSet = [ ];  # Drop all capabilities if possible
     AmbientCapabilities = [ ]; # Ensure this is either not set or explicitly empty
