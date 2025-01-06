5725be31d98d1d7a3539648c5f8176f6bd9fd9a2
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-01-06 11:12:56 +0100

    update: user service hardened to 6.8

--- nixos/core/users/default.nix
+++ nixos/core/users/default.nix
@@ -1,11 +1,42 @@
 {
-  systemd.user.services = {
-    # Apply settings for all user services (user@*)
-    "*".serviceConfig = {
-      NoNewPrivileges = true;
-      PrivateTmp = true;
-      ProtectHome = true;
-      ProtectSystem = "full";
-    };
+  # Allowing system services to be managed in the context of a user's session rather than the system as a whole.
+  systemd.services."user@".serviceConfig = {
+    NoNewPrivileges = true;
+
+    ProtectSystem = "full";
+    # ProtectKernelModules = true;
+    # ProtectControlGroups = true;
+    # ProtectKernelTunables = true;
+    # ProtectKernelLogs = true;
+    # ProtectClock = true; 
+    ProtectHostname = true;
+    # PrivateDevices = true;
+
+    PrivateTmp = true;
+    PrivateNetwork = true;
+
+    # SystemCallFilter = [
+    #   "~@mount"      # Deny mounting operations
+    #   "~@raw-io"     # Deny raw I/O operations
+    #   "~@keyring"    # Deny kernel keyring operations
+    # ];
+
+
+    MemoryDenyWriteExecute = true;
+
+    RestrictAddressFamilies = [ "AF_UNIX" ];
+    RestrictNamespaces = true;
+
+    # ReadOnlyPaths = [ "/etc" "/usr" ];
+
+    # IPAddressDeny = "any";
+
+    # LockPersonality = true;
+
+    # DevicePolicy = "closed";
+
+
+    CapabilityBoundingSet = [ ];  # Drop all capabilities if possible
+    AmbientCapabilities = [ ]; # Ensure this is either not set or explicitly empty
   };
 }
