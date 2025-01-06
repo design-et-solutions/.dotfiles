f36cc1dae90201915bc67ca47b395080e77e27fd
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-01-06 16:34:30 +0100

    fix: ser service hardened to 8.0 (verified)

--- nixos/core/users/default.nix
+++ nixos/core/users/default.nix
@@ -1,34 +1,26 @@
 {
   # Allowing system services to be managed in the context of a user's session rather than the system as a whole.
   systemd.services."user@".serviceConfig = {
-    NoNewPrivileges = true;
+    # NoNewPrivileges = true;
 
-    ProtectSystem = "full";
-    ProtectClock = true; 
-    ProtectHostname = true;
-    ProtectProc = "invisible";
+    # ProtectSystem = "full";
+    # ProtectClock = true; 
+    # ProtectHostname = true;
+    # ProtectProc = "invisible";
 
-    PrivateTmp = true;
-    PrivateNetwork = true;
+    # PrivateTmp = true;
+    # PrivateNetwork = true;
 
-    MemoryDenyWriteExecute = true;
+    # MemoryDenyWriteExecute = true;
 
-    RestrictAddressFamilies = [ "AF_UNIX" ];
+    RestrictAddressFamilies = [ "AF_UNIX" "AF_INET" "AF_INET6" "AF_NETLINK" "AF_BLUETOOTH" ];
     RestrictNamespaces = true;
     RestrictRealtime = true;
     RestrictSUIDSGID = true;
 
     SystemCallArchitectures = "native";
 
-    DevicePolicy = "closed";
-
-    # NoExecPaths = [ "/tmp" "/var/tmp" ];
-    # TemporaryFileSystem = [ "/var:ro" "/opt:ro" ];
-
-    # IPAddressDeny = "any";
-    # IPAddressAllow = [ "127.0.0.1" ];
-
-    CapabilityBoundingSet = [ ];  # Drop all capabilities if possible
-    AmbientCapabilities = [ ]; # Ensure this is either not set or explicitly empty
+    CapabilityBoundingSet = [ ];
+    AmbientCapabilities = [ ];
   };
 }
