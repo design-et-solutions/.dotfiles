6c56ff25db336503727d17a6944a054c4a378af0
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-01-06 21:54:41 +0100

    fix: set service hardened to 6.1 (verified)

--- nixos/core/users/default.nix
+++ nixos/core/users/default.nix
@@ -1,23 +1,25 @@
 {
   # Allowing system services to be managed in the context of a user's session rather than the system as a whole.
   systemd.services."user@".serviceConfig = {
-    # NoNewPrivileges = true;
+    ProtectSystem = "strict";
+    ProtectClock = true; 
+    ProtectHostname = true;
+    ProtectKernelTunables = true;
+    ProtectKernelModules = true;
 
-    # ProtectSystem = "full";
-    # ProtectClock = true; 
-    # ProtectHostname = true;
-    # ProtectProc = "invisible";
+    PrivateTmp = true;
+    PrivateNetwork = true;
 
-    # PrivateTmp = true;
-    # PrivateNetwork = true;
-
-    # MemoryDenyWriteExecute = true;
+    MemoryDenyWriteExecute = true;
 
     RestrictAddressFamilies = [ "AF_UNIX" "AF_INET" "AF_INET6" "AF_NETLINK" "AF_BLUETOOTH" ];
     RestrictNamespaces = true;
     RestrictRealtime = true;
     RestrictSUIDSGID = true;
 
+    SystemCallFilter = [
+      "~@keyring"    # Deny kernel keyring operations
+    ];
     SystemCallArchitectures = "native";
 
     CapabilityBoundingSet = [ ];
