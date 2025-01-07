1719d1eafa70ad0cba2c82135ac1a2d0ba814914
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-01-07 15:57:31 +0100

    add: reload-systemd-vconsole-setup service hardening

--- nixos/core/system/systemd/default.nix
+++ nixos/core/system/systemd/default.nix
@@ -1,31 +1,43 @@
 {
   # Handles reloading the configuration for the Linux virtual console (vconsole)
   systemd.services.reload-systemd-vconsole-setup.serviceConfig = {
-    # nonewprivileges = true;
-    #
-    # protectsystem = "strict"; # mounts everything read-only with the exception of /dev, /proc and /sys
-    # protecthome = true;
-    # protectkernelmodules = true; # prevents explicit kernel module loading
-    # protectnetwork = true;
-    # protectcontrolgroups = true; # protects the linux control groups from modification
-    # protectkerneltunables = true; # prevents kernel tunables from being modified
-    # protectkernellogs = true; # denies access to the kernel log ring buffer
-    # protecthostname = true; # prevents the service from changing the hostname and/or react to changes of the hostname
-    # protectclock = true; # denies all write requests to the hardware clock.
-    #
-    # restrictaddressfamilies = "af_unix af_inet af_inet6 af_netlink"; # restrict the available socket address families
-    # restrictnamespaces = true; # access to namespacing functionality is restricted
-    # restrictrealtime = true; # prevents the service from enabling realtime scheduling policies, which could be used to fully occupy the system
-    # restrictsuidsgid = true; # prevents the setting of the suid or guid on files or directories
-    #
-    # privatetmp = true; # private directory for temporary files
-    # privatedevices = true; # prevents access to physical devices
-    # privateusers = true; # prevents the access to the home directories of other users
-    #
-    # devicepolicy= "closed"; # prevent access to physical devices unless explicitly allowed
-    # memorydenywriteexecute = true; # prevents the creation or modification of memory mappings as executable
-    # lockpersonality = true; # prevents the change of the personality settings for this process
-    # capabilityboundingset = "~cap_linux_immutable cap_ipc_lock cap_sys_chroot cap_block_suspend cap_lease cap_sys_admin cap_sys_boot cap_sys_pacct cap_sys_ptrace cap_sys_rawio cap_sys_time cap_sys_tty_config cap_wake_alarm cap_mac_admin cap_mac_override cap_setuid cap_setgid cap_setpcap cap_chown cap_net_admin cap_fsetid cap_setfcap cap_dac_override cap_dac_read_search cap_fowner cap_ipc_owner"; # disables (via the ‘~’ sign) various potentially dangerous capabilities that this service doesn’t need anyway
+    NoNewPrivileges = true;
+
+    ProtectSystem = "strict";
+    ProtectHome = true;
+    ProtectClock = true; 
+    ProtectHostname = true;
+    ProtectKernelTunables = true;
+    ProtectKernelModules = true;
+    ProtectKernelLogs = true;
+    ProtectProc = "invisible";
+
+    PrivateTmp = true;
+    PrivateMounts = true;
+    PrivateNetwork = true;
+
+    RestrictNamespaces = true;
+    RestrictRealtime = true;
+    RestrictSUIDSGID = true;
+    RestrictAddressFamilies = [ 
+      "~AF_INET6"  
+      "~AF_INET"
+      "~AF_PACKET"
+    ];
+
+    MemoryDenyWriteExecute = true;
+
+    DevicePolicy = "closed";
+
+    LockPersonality = true;
+
+    SystemCallFilter = [
+      "~@keyring"       # Deny kernel keyring operations
+      "~@swap"          # Deny swap operations
+      "~@obsolete"      # Deny system calls outdated, deprecated, or rarely used in modern Linux systems 
+      "~@cpu-emulation" # Deny system calls that are related to CPU state manipulation or virtualization 
+    ];
+    SystemCallArchitectures = "native";
   };
 
   # Prompts for a password or passphrase on the active virtual terminal (VT) where the user is logged in.
@@ -52,6 +64,7 @@
     RestrictAddressFamilies = [ 
       "~AF_INET6"  
       "~AF_INET"
+      "~AF_PACKET"
     ];
 
     MemoryDenyWriteExecute = true;
@@ -94,6 +107,7 @@
     RestrictAddressFamilies = [ 
       "~AF_INET6"  
       "~AF_INET"
+      "~AF_PACKET"
     ];
 
     MemoryDenyWriteExecute = true;
