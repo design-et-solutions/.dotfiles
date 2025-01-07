153d8c55d1695c87286debee3e913d48a72caa57
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-01-07 13:29:21 +0100

    update users service hardening

--- nixos/core/users/default.nix
+++ nixos/core/users/default.nix
@@ -28,10 +28,11 @@
       "~@swap"       # Deny swap operations
       "~@debug"      # Deny debug operations
       "~@module"     # Deny kernel module options
+      "~@obsolete"      # Deny system calls outdated, deprecated, or rarely used in modern Linux systems 
+      "~@cpu-emulation" # Deny system calls that are related to CPU state manipulation or virtualization 
     ];
     SystemCallArchitectures = "native";
 
-    CapabilityBoundingSet = [ ];
-    AmbientCapabilities = [ ];
+    # CapabilityBoundingSet = "~CAP_SYS_ADMIN";
   };
 }
