6c91cbe8087dcfbb08c0c5f9c991a0f4c6a7d46a
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-01-08 17:43:57 +0100

    wip (executable)

--- nixos/optional/gui/default.nix
+++ nixos/optional/gui/default.nix
@@ -26,35 +26,53 @@
   # Managing the graphical display system on your computer.
   systemd.services.display-manager.serviceConfig = {
     ProtectSystem = "full";
-    ProtectKernelTunables = true;
-    ProtectKernelLogs = true;
-    ProtectControlGroups = true;
-    ProtectClock = true;
+    # ProtectKernelLogs = true;
+    # ProtectControlGroups = true;
+    # ProtectClock = true;
 
     PrivateMounts = true;
 
     RestrictSUIDSGID = true;
     RestrictRealtime = true;
+    RestrictNamespaces = [ 
+      # "~uts"
+      # "~mnt" 
+    ];
+    RestrictAddressFamilies = [ 
+      "AF_UNIX"      # Socket family used for inter-process communication (IPC) 
+      "AF_NETLINK"   # Socket family used for communication between user-space applications and the Linux kernel
+      "AF_INET"      # IPv4 internet protocol for regular network communication
+      "AF_INET6"     # IPv6 internet protocol for regular network communication
+      "AF_PACKET"    # Raw packet socket for direct packet-level operations
+    ];
 
     SystemCallFilter = [
-      "~@mount"
-      "~@swap"
-      "~@clock"
       "~@obsolete"
       "~@cpu-emulation"
-      "~@reboot"
+      # "~@clock"
+      # "~@swap"
+      # "~@module"
+      # "~@reboot"
+      # "~@mount"
+
+      # "~@privileged"
+      # "~@debug"
+      # "~@resources"
+      # "~@raw-io"
     ];
     SystemCallArchitectures = "native";
 
-    LockPersonality = true;
+    # LockPersonality = true;
 
-    CapabilityBoundingSet= [
-      "~CAP_SYS_PACCT"
-      "~CAP_SYS_MODULE"
-      "~CAP_BPF"
-      "~CAP_SYS_RAWIO"
-      "~CAP_SYS_BOOT"
-    ];
+    # CapabilityBoundingSet= [
+    #   # "~CAP_SYS_PACCT"
+    #   # "~CAP_SYS_MODULE"
+    #   # "~CAP_BPF"
+    #   # "~CAP_SYS_BOOT"
+    #   # "~CAP_SYS_RAWIO"
+    #   # "~CAP_SYS_CHROOT"
+    #   # "~CAP_BLOCK_SUSPEND"
+    # ];
   };
 
   systemd.services."getty@tty7".serviceConfig = {
