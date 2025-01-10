03d437892a7ca3aba737948430f8f1485393585d
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-01-10 23:24:36 +0100

    add: blocky service service hardening

--- nixos/optional/pkgs/security/blocky/default.nix
+++ nixos/optional/pkgs/security/blocky/default.nix
@@ -28,4 +28,23 @@
       };
     };
   };
+  
+  systemd.services.blocky.serviceConfig = {
+    ProtectKernelTunables = true;
+    ProtectKernelModules = true;
+    ProtectKernelLogs = true;
+    ProtectHostname = true;
+    ProtectControlGroups = true; 
+    ProtectProc = "invisible";
+
+    SystemCallFilter = [
+      "~@obsolete"
+      "~@cpu-emulation"
+      "~@swap"
+      "~@reboot"
+      "~@mount"
+    ];
+    SystemCallArchitectures = "native";
+  };
+
 }
