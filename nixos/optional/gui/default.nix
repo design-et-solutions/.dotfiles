4405452ef087593f3a23e7bb3f34e624ca3e8932
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-01-13 14:35:54 +0100

    wip 5.1

--- nixos/optional/gui/default.nix
+++ nixos/optional/gui/default.nix
@@ -65,76 +65,19 @@
 
     CapabilityBoundingSet = [
       "CAP_SYS_ADMIN" 
-
       "CAP_SETUID"
       "CAP_SETGID"
       "CAP_SETPCAP"
-
       "CAP_DAC_OVERRIDE"
       "CAP_DAC_READ_SEARCH"
       "CAP_FOWNER"
       "CAP_IPC_OWNER"
-
       "CAP_KILL"
-
       "CAP_CHOWN"
       "CAP_FSETID"
       "CAP_SETFCAP"
-
       "CAP_SYS_TTY_CONFIG"
     ];
-    # CapabilityBoundingSet= [
-    #   "~CAP_SYS_PACCT"
-    #   "~CAP_SYS_MODULE"
-    #   "~CAP_SYS_RAWIO"
-    #   "~CAP_MAC_OVERRIDE"
-    #   "~CAP_MAC_ADMIN"
-    #   "~CAP_NET_BIND_SERVICE" 
-    #   "~CAP_NET_BROADCAST"
-    #   "~CAP_NET_RAW"
-    #   "~CAP_SYS_NICE"
-    #   "~CAP_SYS_RESOURCE"
-    #   "~CAP_SYS_PTRACE"
-    #   "~CAP_MKNOD"
-    #   "~CAP_AUDIT_WRITE"
-    #   "~CAP_AUDIT_READ"
-    #   "~CAP_AUDIT_CONTROL"
-    #   "~CAP_NET_ADMIN"
-    #   "~CAP_LEASE"
-    #   "~CAP_SYS_BOOT"
-    #
-    #   # "~CAP_IPC_LOCK"
-    #   # "~CAP_BPF"
-    #   # "~CAP_LINUX_IMMUTABLE"
-    #   # "~CAP_SYS_CHROOT"
-    #   # "~CAP_BLOCK_SUSPEND"
-    #
-    #   # "~CAP_SYSLOG"
-    #
-    #   # "~CAP_IPC_LOCK"
-    #   # "~CAP_BPF"
-    #   # "~CAP_LINUX_IMMUTABLE"
-    #   # "~CAP_SYS_TTY_CONFIG"
-    #   # "~CAP_SYS_CHROOT"
-    #   # "~CAP_BLOCK_SUSPEND"
-    #   # "~CAP_NET_BIND_SERVICE"
-    #   # "~CAP_NET_BROADCAST"
-    #   # "~CAP_NET_RAW" 
-    #
-    #
-    #   # "CAP_DAC_OVERRIDE"
-    #   # "CAP_DAC_READ_SEARCH"
-    #   # "CAP_FOWNER"
-    #   # "CAP_IPC_OWNER"
-    #   # "CAP_KILL"
-    #   # "CAP_SETUID"
-    #   # "CAP_SETGID"
-    #   # "CAP_SETPCAP"
-    #   # "CAP_CHOWN"
-    #   # "CAP_FSETID"
-    #   # "CAP_SETFCAP"
-    #   # "CAP_SYS_ADMIN"
-    # ];
 
     # ProtectKernelTunables = false;
     # ProcSubset = "all";
