59e993e385972c4d8ae0b087426bc1a22d0cc383
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-01-13 20:32:27 +0100

    wip 5.0

--- nixos/optional/gui/default.nix
+++ nixos/optional/gui/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, mergedSetup, ... }:
+{ pkgs, mergedSetup, lib, ... }:
 {
   imports = [
     ../pkgs/tool/thunar 
@@ -13,6 +13,8 @@
           enable = true;
           wayland = true;
         };
+        # gdm = false;
+        # startx.enable = true;
       };
     };
     ratbagd.enable = true; # DBus daemon to configure input devices
@@ -49,37 +51,82 @@
       "~@reboot"
       "~@raw-io"
       "~@debug"
+      # "~@mount"
     ];
     SystemCallArchitectures = "native";
     LockPersonality = true;
     IPAddressDeny = ["0.0.0.0/0" "::/0"];
-    RestrictNamespaces = [ "~cgroup" ];
+    RestrictNamespaces = [ 
+      "~cgroup" 
+      # "~uts"
+      # "~mnt"
+      # "~pid"
+      # "~net"
+    ];
     CapabilityBoundingSet = [
       "CAP_SYS_ADMIN" 
+
       "CAP_SETUID"
       "CAP_SETGID"
-      "CAP_KILL"
-      "CAP_CHOWN"
-      "CAP_SYS_TTY_CONFIG"
       "CAP_SETPCAP"
+
+      "CAP_KILL"
+
+      # "CAP_SYS_TTY_CONFIG"
+
       "CAP_DAC_OVERRIDE"
       "CAP_DAC_READ_SEARCH"
       "CAP_FOWNER"
       "CAP_IPC_OWNER" 
+
       "CAP_FSETID"
       "CAP_SETFCAP"
+      "CAP_CHOWN"
     ];
 
-    SupplementaryGroups = [ "tty" "input" "video" ];
     DeviceAllow = [
-      "/dev/tty1" "rw"           # TTY for login (adjust to your setup)
-      "/dev/tty7" "rw"           # Common TTY for graphical interface
+      "/dev/tty1" "rw"           # TTY for login
+      # "/dev/tty2" "rw"
+      # "/dev/tty3" "rw"
+      "/dev/tty7" "rw"           # TTY for graphical interface 
       "/dev/dri/card*" "rw"      # GPU devices
       "/dev/dri/renderD128" "rw" # Render node
-      "/dev/input/*" "r"         # Input devices
+      "/dev/input/*" "r"          # Input devices (keyboard, mouse, etc.)
+      "/dev/snd/*" "r"            # Audio devices (if sound is required)
+      "/dev/rtc" "r"              # Real-Time Clock (needed for time-related operations)
     ];
     DevicePolicy = "closed";
+
     UMask = 0077;
+    ProtectProc= "default";
+
+    # SecureBits = "keep-caps-locked no-setuid-fixup no-setuid-fixup-locked noroot-locked";
+    # ReadWritePaths = "/run /var/log/nginx";
+    # ExecPaths = "/usr/sbin/nginx /usr/lib";
+    # NoExecPaths = "/";
+    # InaccessiblePaths = "/dev/shm";
+    #
+    # PrivatePIDs = true; 
+    # RemoveIPC = true;
+    # SocketBindDeny = true;
+    # SocketBindAllow = [ "tcp:80" "tcp:443" "udp:443" ];
+    # ProcSubset = "pid";
+
+    # AAAAAAAAHHHHHHHHHHHH
+    # NoNewPrivileges= true;
+
+    # PrivateNetwork=true;
+    # PrivateDevices=true;
+    # PrivateTmp = true;
+    # PrivateUsers=true;
+
+    # ProtectProc= "invisible";
+    # ProtectKernelTunables = true;
+    # ProtectKernelLogs = true;
+    # ProtectHome = true;
+    # ProtectHostname=true;
+
+    # MemoryDenyWriteExecute = true;
   };
 
   systemd.services."getty@tty7".serviceConfig = {
