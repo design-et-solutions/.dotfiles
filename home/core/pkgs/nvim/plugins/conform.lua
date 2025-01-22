5c6e6579f952034341a39f0cbdd0b79ee67e4c80
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-01-22 15:05:07 +0100

    FIX: conform neovim on save

--- home/core/pkgs/nvim/plugins/conform.lua
+++ home/core/pkgs/nvim/plugins/conform.lua
@@ -1,18 +1,25 @@
 require("conform").setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    nix = { "nixfmt" },
-    rust = { "rustfmt", lsp_format = "fallback" },
-    css = { "prettier" },
-    html = { "prettier" },
-    json = { "prettier" },
-    yaml = { "prettier" },
-    markdown = { "prettier" },
-    fish = { "fish_indent" },
-    python = { "isort", "black" },
-    javascript = { "prettierd", "prettier", stop_after_first = true },
-    typescript = { "prettier" },
-    javascriptreact = { "prettier" },
-    typescriptreact = { "prettier" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		nix = { "nixfmt" },
+		rust = { "rustfmt", lsp_format = "fallback" },
+		css = { "prettier" },
+		html = { "prettier" },
+		json = { "prettier" },
+		yaml = { "prettier" },
+		markdown = { "prettier" },
+		fish = { "fish_indent" },
+		python = { "isort", "black" },
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+		typescript = { "prettier" },
+		javascriptreact = { "prettier" },
+		typescriptreact = { "prettier" },
+	},
+	format_on_save = {
+		enabled = true,
+		timeout_ms = 1000,
+		callback = function(bufnr)
+			require("notify")("File formatted successfully: " .. result, "info")
+		end,
+	},
 })
