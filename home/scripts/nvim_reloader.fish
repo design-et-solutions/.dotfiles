c81ea6f75d9e6a701d17d7cedf150831b72b9591
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-01-23 12:27:31 +0100

    UPDATE: home neovim reloader

--- home/scripts/nvim_reloader.fish
+++ home/scripts/nvim_reloader.fish
@@ -1,13 +1,13 @@
 #!/usr/bin/env fish
 
 echo $THEME
-if test "$THEME" = "tokyonight-dark"
-    for server_name in /run/user/1000/nvim.* # maybe should be updated later lol
+if test "$THEME" = tokyonight-dark
+    for server_name in $XDG_RUNTIME_DIR/nvim.* # maybe should be updated later lol
         nvim --server "$server_name" --remote-send ":lua vim.o.background = 'dark'<CR>"
         nvim --server "$server_name" --remote-send ":lua vim.cmd('colorscheme tokyonight-night')<CR>"
     end
-else if test "$THEME" = "gruvbox-light"
-    for server_name in /run/user/1000/nvim.* # maybe should be updated later lol
+else if test "$THEME" = gruvbox-light
+    for server_name in $XDG_RUNTIME_DIR/nvim.* # maybe should be updated later lol
         nvim --server "$server_name" --remote-send ":lua vim.o.background = 'light'<CR>"
         nvim --server "$server_name" --remote-send ":lua vim.cmd('colorscheme gruvbox')<CR>"
         # double to avoid color issue
