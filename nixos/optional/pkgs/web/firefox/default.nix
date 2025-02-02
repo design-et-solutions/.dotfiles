f40847d2475e08ad7a93f9170657e40006f87ac2
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-02-02 23:47:41 +0100

    UPGRADE: firefox

--- nixos/optional/pkgs/web/firefox/default.nix
+++ nixos/optional/pkgs/web/firefox/default.nix
@@ -10,63 +10,159 @@
       "en-US"
     ];
     policies = {
-      DefaultDownloadDirectory = "~/Downloads";
-      DisableTelemetry = true;
-      DisableFirefoxStudies = true;
+      DefaultDownloadDirectory = "~/Downloads"; # Set the default download directory.
       EnableTrackingProtection = {
         Value = true;
         Locked = true;
         Cryptomining = true;
         Fingerprinting = true;
+      }; # Configure tracking protection.
+      DisableTelemetry = true; # Prevent the upload of telemetry data.
+      DisableFirefoxStudies = true; # Disable Firefox studies (Shield).
+      DisablePocket = true; # Remove Pocket in the Firefox UI.
+      DisableFirefoxAccounts = true; # Disable Firefox Accounts integration (Sync).
+      DisableFirefoxScreenshots = true; # Remove access to Firefox Screenshots.
+      OverrideFirstRunPage = ""; # Override the first run page. If the value is an empty string (“”), the first run page is not displayed.
+      OverridePostUpdatePage = ""; # Override the upgrade page. If the value is an empty string (“”), no extra pages are displayed when Firefox is upgraded.
+      DontCheckDefaultBrowser = true; # Don’t check if Firefox is the default browser at startup.
+      # BlockAboutConfig = true; # Block access to about:config.
+      DisablePasswordReveal = true; # Do not allow passwords to be shown in saved logins.
+      PrimaryPassword = true; # Require or prevent using a primary (formerly master) password.
+      HttpsOnlyMode = "enabled"; # Configure HTTPS-Only Mode.
+      DNSOverHTTPS = {
+        Enabled = true;
+        ProviderURL = "https://mozilla.cloudflare-dns.com/dns-query";
+        Locked = true;
+      }; # Configure DNS over HTTPS (DoH).
+      SearchBar = "separate"; # Set whether or not search bar is displayed.
+      SearchEngines = {
+        Default = "DuckDuckGo"; # Set the default search engine. This policy is only available on the ESR.
+        Remove = [ "Google" ];
+        PreventInstalls = false; # Prevent installing search engines from webpages.
       };
-      DisablePocket = true;
-      DisableFirefoxAccounts = true;
-      DisableAccounts = true;
-      DisableFirefoxScreenshots = true;
-      OverrideFirstRunPage = "";
-      OverridePostUpdatePage = "";
-      DontCheckDefaultBrowser = true;
-      # BlockAboutConfig = true;
-      # ExtensionSettings = {
-      #   "*".installation_mode = "blocked"; # blocks all addons except below
-      #   "vimium-ff@vimium.com" = {
-      #     install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
-      #     installation_mode = "force_installed";
-      #   };
-      # };
+      ExtensionSettings = {
+        "*".installation_mode = "blocked"; # blocks all addons except below
+        "jid1-MnnxcxisBPnSXQ@jetpack" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+          installation_mode = "force_installed";
+        }; # Automatically learns to block invisible trackers.
+        "jid1-ZAdIEUB7XOzOJw@jetpack" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/duckduckgo-for-firefox/latest.xpi";
+          installation_mode = "force_installed";
+        }; # Simple and seamless privacy protection for your browser: tracker blocking, cookie protection, DuckDuckGo private search, email protection, HTTPS upgrading, and much more.
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        }; # Wide-spectrum content blocker.
+        "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
+          installation_mode = "force_installed";
+        }; # The Hacker's Browser.
+        "newtaboverride@agenedia.com" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/new-tab-override/latest.xpi";
+          installation_mode = "force_installed";
+        }; # New Tab Override allows you to set the page that shows whenever you open a new tab.
+      }; # Manage all aspects of extensions.
       Preferences = {
+        "browser.theme.content-theme" = 0;
         "browser.startup.homepage" = "https://homepage-brown-ten.vercel.app/";
-        "extensions.screenshots.disabled" = true;
-        # "browser.bookmarks.managed.enabled" = true;
-        # extensions = [
-        #   (pkgs.fetchFirefoxAddon {
-        #     name = "vimium";
-        #     url = "https://addons.mozilla.org/firefox/downloads/file/4259790/vimium_ff-2.1.2.xpi";
-        #     sha256 = "sha256-O51D7id/83TjsRU/l9wgywbmVBFqgzZ0x5tDuIh4IOE=";
-        #   })
-        # ];
-        # "font.name.monospace.x-western" = "FiraCode Nerd";
-        # "font.name.sans-serif.x-western" = "FiraCode Nerd";
-        # "font.name.serif.x-western" = "FiraCode Nerd";
-        # "browser.bookmarks.managed.enabled" = { Value = true; Status = "locked"; };
-        # "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
-        # "browser.topsites.contile.enabled" = { Value = false; Status = "locked"; };
-        # "browser.formfill.enable" = { Value = false; Status = "locked"; };
-        # "browser.search.suggest.enabled" = { Value = false; Status = "locked"; };
-        # "browser.search.suggest.enabled.private" = { Value = false; Status = "locked"; };
-        # "browser.urlbar.suggest.searches" = { Value = false; Status = "locked"; };
-        # "browser.urlbar.showSearchSuggestionsFirst" = { Value = false; Status = "locked"; };
-        # "browser.newtabpage.activity-stream.feeds.section.topstories" = { Value = false; Status = "locked"; };
-        # "browser.newtabpage.activity-stream.feeds.snippets" = { Value = false; Status = "locked"; };
-        # "browser.newtabpage.activity-stream.section.highlights.includePocket" = { Value = false; Status = "locked"; };
-        # "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = { Value = false; Status = "locked"; };
-        # "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = { Value = false; Status = "locked"; };
-        # "browser.newtabpage.activity-stream.section.highlights.includeVisited" = { Value = false; Status = "locked"; };
-        # "browser.newtabpage.activity-stream.showSponsored" = { Value = false; Status = "locked"; };
-        # "browser.newtabpage.activity-stream.system.showSponsored" = { Value = false; Status = "locked"; };
-        # "browser.newtabpage.activity-stream.showSponsoredTopSites" = { Value = false; Status = "locked"; };
-        # "extensions.pocket.enabled" = { Value = false; Status = "locked"; };
-        # "extensions.screenshots.disabled" = { Value = true; Status = "locked"; };
+        "browser.urlbar.suggest.searches" = {
+          Value = false;
+          Status = "locked";
+        };
+        "browser.urlbar.showSearchSuggestionsFirst" = {
+          Value = false;
+          Status = "locked";
+        };
+        "browser.search.suggest.enabled" = {
+          Value = false;
+          Status = "locked";
+        };
+        "browser.search.suggest.enabled.private" = {
+          Value = false;
+          Status = "locked";
+        };
+        "browser.formfill.enable" = {
+          Value = false;
+          Status = "locked";
+        };
+        "browser.contentblocking.category" = {
+          Value = "strict";
+          Status = "locked";
+        };
+        "browser.topsites.contile.enabled" = {
+          Value = false;
+          Status = "locked";
+        };
+        "browser.newtabpage.activity-stream.feeds.section.topstories" = {
+          Value = false;
+          Status = "locked";
+        };
+        "browser.newtabpage.activity-stream.feeds.snippets" = {
+          Value = false;
+          Status = "locked";
+        };
+        "browser.newtabpage.activity-stream.section.highlights.includePocket" = {
+          Value = false;
+          Status = "locked";
+        };
+        "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = {
+          Value = false;
+          Status = "locked";
+        };
+        "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = {
+          Value = false;
+          Status = "locked";
+        };
+        "browser.newtabpage.activity-stream.section.highlights.includeVisited" = {
+          Value = false;
+          Status = "locked";
+        };
+        "browser.newtabpage.activity-stream.showSponsored" = {
+          Value = false;
+          Status = "locked";
+        };
+        "browser.newtabpage.activity-stream.system.showSponsored" = {
+          Value = false;
+          Status = "locked";
+        };
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = {
+          Value = false;
+          Status = "locked";
+        };
+        "extensions.screenshots.disabled" = {
+          Value = true;
+          Status = "locked";
+        };
+        "extensions.pocket.enabled" = {
+          Value = false;
+          Status = "locked";
+        };
+        "identity.fxaccounts.enabled" = {
+          Value = false;
+          Status = "locked";
+        };
+        "media.peerconnection.enabled" = {
+          Value = false;
+          Status = "locked";
+        };
+        "reader.parse-on-load.enabled" = {
+          Value = true;
+          Status = "locked";
+        };
+        "zoom.defaultPercent" = 100;
+        "privacy.resistFingerprinting" = {
+          Value = true;
+          Status = "locked";
+        };
+        "privacy.trackingprotection.fingerprinting.enabled" = {
+          Value = true;
+          Status = "locked";
+        };
+        "privacy.trackingprotection.cryptomining.enabled" = {
+          Value = true;
+          Status = "locked";
+        };
       };
     };
   };
