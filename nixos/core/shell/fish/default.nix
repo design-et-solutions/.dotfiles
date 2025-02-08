50d92391154a5d9030e5a8de05c15db54edec6e3
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-02-08 17:55:50 +0100

    ADD: jq

--- nixos/core/shell/fish/default.nix
+++ nixos/core/shell/fish/default.nix
@@ -8,35 +8,35 @@
       la = "ls -a";
 
       # GITHUB
-      gh = "lazygit"; 
-      gh-reset-last-commit= "git commit --amend";
+      gh = "lazygit";
+      gh-reset-last-commit = "git commit --amend";
       gh-stash = "git stash";
       gh-stash-back = "git stash pop";
       gh-tag = "git tag";
-      
+
       # RUST
-      clippy-full = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::all -W clippy::pedantic -W clippy::nursery -W clippy::perf -W clippy::complexity -W clippy::suspicious -W clippy::style -W clippy::correctness"; 
+      clippy-full = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::all -W clippy::pedantic -W clippy::nursery -W clippy::perf -W clippy::complexity -W clippy::suspicious -W clippy::style -W clippy::correctness";
       # all lints that are on by default (correctness, suspicious, style, complexity, perf)
-      clippy-all = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::all"; 
+      clippy-all = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::all";
       # lints for the cargo manifest
-      clippy-cargo = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::cargo"; 
+      clippy-cargo = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::cargo";
       # new lints that are still under development
-      clippy-nursery = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::nursery"; 
+      clippy-nursery = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::nursery";
       # lints which are rather strict or have occasional false positives
-      clippy-pedantic = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::pedantic"; 
+      clippy-pedantic = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::pedantic";
       # code that can be written to run faster
-      clippy-perf = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::perf"; 
+      clippy-perf = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::perf";
       # lints which prevent the use of language and library features1
-      clippy-restriction = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::restriction"; 
+      clippy-restriction = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::restriction";
       # code that does something simple but in a complex way
-      clippy-complexity = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::complexity"; 
+      clippy-complexity = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::complexity";
       # code that should be written in a more idiomatic way
-      clippy-style = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::style"; 
+      clippy-style = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::style";
       # code that is most likely wrong or useless
-      clippy-suspicious = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::suspicious"; 
+      clippy-suspicious = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::suspicious";
       # code that is outright wrong or useless
-      clippy-correctness = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::correctness"; 
-      cargo-check-udeps = "rustup run nightly cargo udeps --all-targets  --all-features"; 
+      clippy-correctness = "cargo clippy --all-targets --all-features -- -D warnings -W clippy::correctness";
+      cargo-check-udeps = "rustup run nightly cargo udeps --all-targets  --all-features";
       cargo-tree-search = "cargo tree -i ";
     };
     interactiveShellInit = ''
@@ -55,7 +55,6 @@
     enable = true;
   };
 
-
   environment.systemPackages = with pkgs; [
     fish
     starship
@@ -65,6 +64,7 @@
     fzf
     fishPlugins.grc
     grc
+    jq
   ];
 
   environment.shells = with pkgs; [ fish ];
