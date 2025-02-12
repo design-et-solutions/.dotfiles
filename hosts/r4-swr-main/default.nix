160aa014ed1e3a289175d65aef4bfe26ef62b1b9
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-02-12 12:36:43 +0100

    FIX

--- hosts/r4-swr-main/default.nix
+++ hosts/r4-swr-main/default.nix
@@ -58,7 +58,7 @@
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       WorkingDirectory = "/home/me/poc/rtsp-client";
-      ExecStart = "${pkgs.nodePackages}/bin/npm start";
+      ExecStart = "${pkgs.nodejs}/bin/npm start";
       Restart = "always";
       RestartSec = "5s";
     };
