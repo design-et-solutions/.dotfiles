0fe6424c616ca8f32061195371d955d07c0bf75f
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-02-20 16:44:51 +0100

    FIX

--- hosts/r4-swr-main/orange.nix
+++ hosts/r4-swr-main/orange.nix
@@ -8,8 +8,8 @@
         "/home/me/4757-R4-SWR/orange/DOTenv"
         "/home/me/4757-R4-SWR/orange/credentials"
       ];
-      ExecStartPre = "${pkgs.bash}/bin/bash -c 'echo ${DOCK_PASS} | ${pkgs.docker}/bin/docker login -u ${DOCK_USER} --password-stdin ${DOCK_REMOTE}'";
-      ExecStart = "${pkgs.docker-compose}/bin/docker-compose --project-name ${DOCK_PROJECT} -f /home/me/4757-R4-SWR/orange/docker-compose-v2x-edge-node.yml up";
+      ExecStartPre = "${pkgs.bash}/bin/bash -c 'echo $DOCK_PASS | ${pkgs.docker}/bin/docker login -u $DOCK_USER --password-stdin $DOCK_REMOTE'";
+      ExecStart = "${pkgs.docker-compose}/bin/docker-compose --project-name $DOCK_PROJECT -f /home/me/4757-R4-SWR/orange/docker-compose-v2x-edge-node.yml up";
       ExecStartPost = "${pkgs.docker}/bin/docker logout";
       Restart = "always";
       RestartSec = "5s";
@@ -24,8 +24,8 @@
         "/home/me/4757-R4-SWR/orange/DOTenv"
         "/home/me/4757-R4-SWR/orange/credentials"
       ];
-      ExecStartPre = "${pkgs.bash}/bin/bash -c 'echo ${DOCK_PASS} | ${pkgs.docker}/bin/docker login -u ${DOCK_USER} --password-stdin ${DOCK_REMOTE}'";
-      ExecStart = "${pkgs.docker-compose}/bin/docker-compose --project-name ${DOCK_PROJECT} -f /home/me/4757-R4-SWR/orange/docker-compose-allog.yml up";
+      ExecStartPre = "${pkgs.bash}/bin/bash -c 'echo $DOCK_PASS | ${pkgs.docker}/bin/docker login -u $DOCK_USER --password-stdin $DOCK_REMOTE'";
+      ExecStart = "${pkgs.docker-compose}/bin/docker-compose --project-name $DOCK_PROJECT -f /home/me/4757-R4-SWR/orange/docker-compose-allog.yml up";
       ExecStartPost = "${pkgs.docker}/bin/docker logout";
       Restart = "always";
       RestartSec = "5s";
