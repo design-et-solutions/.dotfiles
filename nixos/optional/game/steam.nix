ff82b9fe114fb946b1addf7d64dc617c432564f2
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-02-21 14:57:35 +0100

    FIX

--- nixos/optional/game/steam.nix
+++ nixos/optional/game/steam.nix
@@ -1,18 +1,8 @@
 {
-  lib,
-  pkgs,
-  config,
   ...
 }:
 {
-  # programs.steam = {
-  #   enable = true;
-  #   remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-  #   dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  #   localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  # };
-
-  environment.systemPackages = with pkgs; [
-    steam
-  ];
+  programs.steam = {
+    enable = true;
+  };
 }
