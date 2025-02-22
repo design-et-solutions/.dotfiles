c6f04e2308e28d495ebdc45121843b834ee7bf44
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-02-22 22:06:23 +0100

    FMT

--- nixos/optional/networking/vpn/client.nix
+++ nixos/optional/networking/vpn/client.nix
@@ -1,31 +1,36 @@
-{ pkgs, is_external, ... }:
+{ pkgs, isExternal, ... }:
 {
   imports = [
     ./default.nix
   ];
 
-  networking= {
-   wg-quick = let
-      peerPublicKey = "YTdToContsMCnJUr9yglcfd5T59YISBdRpRZ1w3IiH8=";
-      privateKeyFilePath = "/etc/wireguard/wg0";
-    in {
-      interfaces = {
-        wg0 = {
-          listenPort = 51820;
-          privateKeyFile = privateKeyFilePath;
-          postUp = ''
-            wg set wg0 peer ${peerPublicKey} persistent-keepalive 25
-          '';
-          peers = [
-            {
-              publicKey = peerPublicKey;
-              # Forward all the traffic via VPN.
-              allowedIPs = [ "0.0.0.0/0" "::/0" ];
-              endpoint = if is_external then "128.78.107.5:51820" else "192.168.10.213:51820";
-            }
-          ];
+  networking = {
+    wg-quick =
+      let
+        peerPublicKey = "YTdToContsMCnJUr9yglcfd5T59YISBdRpRZ1w3IiH8=";
+        privateKeyFilePath = "/etc/wireguard/wg0";
+      in
+      {
+        interfaces = {
+          wg0 = {
+            listenPort = 51820;
+            privateKeyFile = privateKeyFilePath;
+            postUp = ''
+              wg set wg0 peer ${peerPublicKey} persistent-keepalive 25
+            '';
+            peers = [
+              {
+                publicKey = peerPublicKey;
+                # Forward all the traffic via VPN.
+                allowedIPs = [
+                  "0.0.0.0/0"
+                  "::/0"
+                ];
+                endpoint = if isExternal then "128.78.107.5:51820" else "192.168.10.213:51820";
+              }
+            ];
+          };
         };
       };
-    };
   };
 }
