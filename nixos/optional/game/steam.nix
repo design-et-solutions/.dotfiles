bf9d710db28acb9e1bea47799ef63bb065c1954f
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-02-22 23:47:34 +0100

    FIX: steam

--- nixos/optional/game/steam.nix
+++ nixos/optional/game/steam.nix
@@ -1,8 +1,15 @@
-{
-  ...
-}:
+{ pkgs, ... }:
 {
   programs.steam = {
     enable = true;
+    gamescopeSession.enable = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    mangohud
+    protonup
+    lutris
+  ];
+
+  programs.gamemode.enable = true;
 }
