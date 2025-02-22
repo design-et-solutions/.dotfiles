e782a4870682ec056562286e11f9aaa47d71d472
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-02-22 17:09:50 +0100

    ADD: new host laptop-work

---
+++ hosts/laptop-work/default.nix
@@ -0,0 +1,28 @@
+{ pkgs, lib, ... }:
+let
+  name = "laptop-work";
+in
+{
+  imports = [
+    # Import your generated (nixos-generate-config) hardware configuration
+    ./hardware-configuration.nix
+  ];
+
+  time.timeZone = "Europe/Paris";
+
+  networking = {
+    hostName = name;
+    # wg-quick = {
+    #   interfaces = {
+    #     wg0 = {
+    #       address = [ "10.100.0.5/32" ];
+    #     };
+    #   };
+    # };
+  };
+
+  # environment.etc."wireguard/wg0" = {
+  #   source = builtins.toString ../../secrets/${name}/wg0;
+  #   mode = "0400";
+  # };
+}
---
+++ hosts/laptop-work/host.nix
@@ -0,0 +1,61 @@
+{ mkNixosConfiguration, ... }:
+
+mkNixosConfiguration {
+  system = "x86_64-linux";
+  host = ./.;
+  users = [ "me" ];
+  setup = {
+    gui.enable = true;
+    browser.enable = true;
+    file_explorer.enable = true;
+    mail.enable = true;
+    print.enable = true;
+    vm.enable = true;
+    game = {
+      steam.enable = true;
+    };
+    social = {
+      discord.enable = true;
+      slack.enable = true;
+      teams.enable = true;
+      whatsapp.enable = true;
+    };
+    misc = {
+      solaar.enable = true;
+      handbrake.enable = true;
+      gimp.enable = true;
+      vial.enable = true;
+      drawio.enable = true;
+      steam-run.enable = true;
+      stremio.enable = true;
+    };
+    video = {
+      vlc.enable = true;
+      mpv.enable = true;
+    };
+    audio = {
+      default.enable = true;
+      spotify.enable = true;
+    };
+    networking = {
+      analyzer.enable = true;
+      # vpn = {
+      #   default.enable = true;
+      #   client.enable = true;
+      #   is_external = true;
+      # };
+      wifi = {
+        emergency.enable = true;
+      };
+      bluetooth.enable = true;
+      # can = {
+      #   default.enable = true;
+      #   peak.enable = true;
+      # };
+    };
+    security = {
+      blocker.enable = true;
+      analyzer.enable = true;
+    };
+  };
+}
