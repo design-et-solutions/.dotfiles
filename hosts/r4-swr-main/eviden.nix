ad045efa9a94299a46d5e9d36adb530357e9c8f8
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-02-27 15:51:17 +0100

    FIX

--- hosts/r4-swr-main/eviden.nix
+++ hosts/r4-swr-main/eviden.nix
@@ -13,24 +13,9 @@
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       User = "me";
-      # Group = "vboxusers";
-      # VBOX_USER_HOME = /home/me/.config/VirtualBox;
-      # ExecStartPre = [
-      # "${pkgs.kmod}/bin/modprobe vboxdrv"
-      # "${pkgs.kmod}/bin/modprobe vboxnetflt"
-      # "${pkgs.kmod}/bin/modprobe vboxnetadp"
-      # ];
-      # ExecStart = "${pkgs.virtualbox}/bin/VBoxHeadless --comment 'synergy' -startvm 'synergy' --vrde config";
-      ExecStart = "/run/current-system/sw/bin/bash -l -c '${pkgs.virtualbox}/bin/VBoxHeadless --comment \"synergy\" -startvm \"synergy\" --vrde config'";
+      ExecStart = "${pkgs.virtualbox}/bin/VBoxHeadless --comment 'synergy' -startvm 'synergy' --vrde config";
       Restart = "always";
       RestartSec = "5s";
-      Environment = [
-        "PATH=${pkgs.virtualbox}/bin:${pkgs.coreutils}/bin:/usr/bin:/bin"
-      ];
     };
   };
-
-  # services.udev.extraRules = ''
-  # KERNEL=="vboxdrv", GROUP="vboxusers", MODE="0660"
-  # '';
 }
