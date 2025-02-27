ef317c09ad234923b7f6b3fc1ef40cea23abb79a
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-02-27 10:38:59 +0100

    ADD: gstreamer

--- hosts/r4-swr-main/eviden.nix
+++ hosts/r4-swr-main/eviden.nix
@@ -5,6 +5,11 @@
   environment.systemPackages = with pkgs; [
     bash
     virtualbox
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-libav
   ];
 
   users.extraGroups.vboxusers.members = [ "me" ];
