49a4856f8169d7df99e8cef4b047368043937e3c
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-02-27 15:37:32 +0100

    FIX

--- hosts/r4-swr-main/eviden.nix
+++ hosts/r4-swr-main/eviden.nix
@@ -12,16 +12,10 @@
     description = "Run Synergy VirtualBox";
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      User = "me";
+      Group = "vboxusers";
       ExecStart = "${pkgs.virtualbox}/bin/VBoxHeadless --comment 'synergy' -startvm 'synergy' --vrde config";
-      # ExecStart = "${pkgs.virtualbox}/bin/VirtualBox /home/me/VirtualBox\ VMs/synergy/synergy.vbox";
-      # ExecStart = "${pkgs.virtualbox}/bin/VBoxManage startvm \"synergy\" --type headless && ${pkgs.virtualbox}/bin/VBoxManage controlvm \"synergy\" setcredentials \"synergy\" \"synergy\" \"domain\"";
       Restart = "always";
       RestartSec = "5s";
-      Environment = [
-        "WAYLAND_DISPLAY=wayland-1"
-        "XDG_RUNTIME_DIR=/run/user/1000"
-      ];
     };
   };
 }
