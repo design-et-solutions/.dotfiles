2e8940f70e59aef550407900356303355cfa4ba8
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-02-28 10:22:21 +0100

    FIX

--- hosts/r4-swr-main/eviden.nix
+++ hosts/r4-swr-main/eviden.nix
@@ -13,7 +13,7 @@
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       User = "rooot";
-      ExecStart = "/run/wrappers/bin/VBoxHeadlesss --comment 'synergy' -startvm 'synergy' --vrde config";
+      ExecStart = "/run/wrappers/bin/VBoxHeadless --comment 'synergy' -startvm 'synergy' --vrde config";
       Restart = "always";
       RestartSec = "5s";
     };
