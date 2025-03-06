0678f7f8b4a7c3a63411595889aecc979ff29289
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-06 16:31:49 +0100

    TRY orange fix

--- hosts/r4-swr-main/orange.nix
+++ hosts/r4-swr-main/orange.nix
@@ -1,34 +1,36 @@
 { pkgs, ... }:
 {
-  systemd.services."docker-compose-v2x-edge-node" = {
-    description = "Run Docker for v2x edge node";
+  # Login
+  # docker login platform-mce.orange-labs.fr:2424
+  # softwrep/worldWild
+
+  # docker image ls
+  # docker rmi $(docker images -a -q)
+  # docker rm if not able to rmi
+
+  systemd.services."orange-golem-swr" = {
+    description = "Run Docker golem-swr";
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      EnvironmentFile = [
-        "/home/me/4757-R4-SWR/orange-storage/DOTenv"
-        "/home/me/4757-R4-SWR/orange-storage/credentials"
-      ];
-      ExecStartPre = "${pkgs.bash}/bin/bash -c '${pkgs.coreutils}/bin/echo $DOCK_PASS | ${pkgs.docker}/bin/docker login -u $DOCK_USER --password-stdin $DOCK_REMOTE'";
-      ExecStart = "${pkgs.docker-compose}/bin/docker-compose --project-name $DOCK_PROJECT -f /home/me/4757-R4-SWR/orange-storage/docker-compose-v2x-edge-node.yml up";
-      # ExecStart = "${pkgs.docker-compose}/bin/docker-compose --project-name $DOCK_PROJECT -f /home/me/4757-R4-SWR/orange-storage/docker-compose-v2x-edge-node.yml pull && ${pkgs.docker-compose}/bin/docker-compose --project-name $DOCK_PROJECT -f /home/me/4757-R4-SWR/orange-storage/docker-compose-v2x-edge-node.yml up";
-      ExecStartPost = "${pkgs.docker}/bin/docker logout";
+      ExecStart = "docker run -d platform-mce.orange-labs.fr:2424/orange/golem_swr:latest";
       Restart = "always";
       RestartSec = "5s";
     };
   };
-
-  systemd.services."docker-compose-allog" = {
-    description = "Run Docker for allog";
+  systemd.services."orange-allog-swr" = {
+    description = "Run Docker allog-swr";
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      EnvironmentFile = [
-        "/home/me/4757-R4-SWR/orange-storage/DOTenv"
-        "/home/me/4757-R4-SWR/orange-storage/credentials"
-      ];
-      ExecStartPre = "${pkgs.bash}/bin/bash -c '${pkgs.coreutils}/bin/echo $DOCK_PASS | ${pkgs.docker}/bin/docker login -u $DOCK_USER --password-stdin $DOCK_REMOTE'";
-      ExecStart = "${pkgs.docker-compose}/bin/docker-compose --project-name $DOCK_PROJECT -f /home/me/4757-R4-SWR/orange-storage/docker-compose-allog.yml up";
-      # ExecStart = "${pkgs.docker-compose}/bin/docker-compose --project-name $DOCK_PROJECT -f /home/me/4757-R4-SWR/orange-storage/docker-compose-allog.yml pull && ${pkgs.docker-compose}/bin/docker-compose --project-name $DOCK_PROJECT -f /home/me/4757-R4-SWR/orange-storage/docker-compose-allog.yml up";
-      ExecStartPost = "${pkgs.docker}/bin/docker logout";
+      ExecStart = "docker run -d platform-mce.orange-labs.fr:2424/orange/orange_allog_swr:latest";
+      Restart = "always";
+      RestartSec = "5s";
+    };
+  };
+  systemd.services."orange-mosquitto-swr" = {
+    description = "Run Docker mosquitto-swr";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "docker run -d platform-mce.orange-labs.fr:2424/orange/orange_mosquitto_swr:latest";
       Restart = "always";
       RestartSec = "5s";
     };
