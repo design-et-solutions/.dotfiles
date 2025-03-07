b4f32e32765058b15b253dfcaaee62c392e40983
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-07 13:43:39 +0100

    WIP

--- nixos/optional/gui/default.nix
+++ nixos/optional/gui/default.nix
@@ -11,6 +11,7 @@ in
   imports = [
     ./hyprland.nix
     ./wayland.nix
+    ./gnome.nix
   ];
 
   services = {
@@ -23,12 +24,11 @@ in
       displayManager = {
         gdm = {
           enable = true;
-          wayland = if mergedSetup.gui.params.displayServer == "wayland" then true else false;
+          wayland = if isWayland then true else false;
           banner = "go fuck your self";
         };
       };
-      desktopManager.gnome.enable =
-        if mergedSetup.gui.params.displayServer == "wayland" then false else true;
+      desktopManager.gnome.enable = if isWayland then false else true;
     };
     ratbagd.enable = true; # DBus daemon to configure input devices
     dbus.enable = true;
@@ -38,84 +38,6 @@ in
     enable = true;
     enable32Bit = true;
   };
-
-  # Managing the graphical display system on your computer.
-  # systemd.services.display-manager.serviceConfig = {
-  #   NoNewPrivileges = false;
-  #
-  #   ProtectSystem = "full";
-  #   ProtectControlGroups = true;
-  #   ProtectClock = true;
-  #   ProtectKernelModules = true;
-  #   ProtectProc = "default";
-  #
-  #   PrivateMounts = true;
-  #   PrivateIPC = true;
-  #
-  #   RestrictSUIDSGID = true;
-  #   RestrictRealtime = true;
-  #   RestrictFileSystems = [ ];
-  #   RestrictNamespaces = [
-  #     "~cgroup"
-  #     "uts"
-  #     "pid"
-  #     "net"
-  #     "user"
-  #     "mnt"
-  #     "ipc"
-  #   ];
-  #   RestrictAddressFamilies = [
-  #     "AF_UNIX"
-  #     "AF_NETLINK"
-  #     "AF_INET"
-  #     "AF_INET6"
-  #     "~AF_PACKET"
-  #   ];
-  #
-  #   SystemCallArchitectures = [ "native" ];
-  #   SystemCallErrorNumber = "EPERM";
-  #   SystemCallFilter = [
-  #     "~@obsolete"
-  #     "~@cpu-emulation"
-  #     "~@clock"
-  #     "~@swap"
-  #     "~@module"
-  #     "~@reboot"
-  #     "~@raw-io"
-  #     "~@debug"
-  #   ];
-  #
-  #   CapabilityBoundingSet = [
-  #     "CAP_SYS_ADMIN"
-  #     "CAP_SETUID"
-  #     "CAP_SETGID"
-  #     "CAP_SETPCAP"
-  #     "CAP_KILL"
-  #     "CAP_SYS_TTY_CONFIG"
-  #     "CAP_DAC_OVERRIDE"
-  #     "CAP_DAC_READ_SEARCH"
-  #     "CAP_FOWNER"
-  #     "CAP_IPC_OWNER"
-  #     "CAP_FSETID"
-  #     "CAP_SETFCAP"
-  #     "CAP_CHOWN"
-  #   ];
-  #   AmbientCapabilities = [ ];
-  #
-  #   DevicePolicy = "closed";
-  #   DeviceAllow = [
-  #     "/dev/tty7 rw"
-  #     "/dev/input/* rw" # Allow Wayland to access keyboards/mice
-  #     "/dev/dri/* rw" # Allow access to GPU devices
-  #   ];
-  #
-  #   LockPersonality = true;
-  #   UMask = "077";
-  #   IPAddressDeny = "any";
-  #   KeyringMode = lib.mkForce "private";
-  #   User = "root";
-  #   Group = "root";
-  # };
 
   environment.systemPackages = with pkgs; [
     brightnessctl # Command-line utility to control device brightness
