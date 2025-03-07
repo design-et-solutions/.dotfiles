568efbc0669a432596adad6c938daae658758157
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-07 15:21:31 +0100

    FIX

---
+++ hosts/r4-swr-main/test.nix
@@ -0,0 +1,38 @@
+{ pkgs, ... }:
+{
+  systemd.services."test-rtsp-server" = {
+    description = "Run RTSP Server";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      WorkingDirectory = "/home/me/poc/rtsp-server";
+      ExecStart = "/run/current-system/sw/bin/nix-shell shell.nix --run './target/release/main'";
+      Restart = "always";
+      RestartSec = "5s";
+    };
+  };
+
+  systemd.services."test-ws-server" = {
+    description = "Run WS Server";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      User = "me";
+      WorkingDirectory = "/home/me/poc/rtsp-client";
+      ExecStart = "/run/current-system/sw/bin/nix-shell shell.nix --pure --run 'node stream.js'";
+      Restart = "always";
+      RestartSec = "5s";
+    };
+  };
+
+  systemd.services."test-client" = {
+    description = "Run React Client";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      User = "me";
+      WorkingDirectory = "/home/me/poc/rtsp-client";
+      ExecStart = "${pkgs.nodejs}/bin/npm start -- -p 3001";
+      Restart = "always";
+      RestartSec = "5s";
+      Environment = "PATH=/run/current-system/sw/bin:/bin:/usr/bin";
+    };
+  };
+}
