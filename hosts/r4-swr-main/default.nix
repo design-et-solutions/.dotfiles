2ccb05011d36cf8330b5172de6dcb8fd8f826a9e
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-07 15:16:50 +0100

    FIX

--- hosts/r4-swr-main/default.nix
+++ hosts/r4-swr-main/default.nix
@@ -14,5 +14,17 @@
 
   networking = {
     hostName = "r4-swr-main";
+    # hosts = {
+    #   "192.100.1.1" = [ "cdp.thales" ];
+    # };
+    static = {
+      routes = [
+        {
+          address = "192.100.1.0";
+          prefixLength = 24;
+          via = "192.168.100.1";
+        }
+      ];
+    };
   };
 }
