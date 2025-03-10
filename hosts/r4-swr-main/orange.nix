c61fdff571c63787b96be9c92c871beedd8acc81
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-10 12:24:33 +0100

    ADD: orange instruction

--- hosts/r4-swr-main/orange.nix
+++ hosts/r4-swr-main/orange.nix
@@ -11,6 +11,10 @@
   # docker-compose --project-name swr -f /home/me/4757-R4-SWR/orange-storage/docker-compose-allog.yml pull
   # docker-compose --project-name swr -f /home/me/4757-R4-SWR/orange-storage/docker-compose-v2x-edge-node pull
 
+  #   Pour la mise à jour il faut donc arrêter les containers, puis supprimer les volumes:
+  # docker volume rm swr_mqttlog swr_orange-golem-conf-swr swr_orange-golem-log-swr swr_orange-mosquitto-conf swr_orange-mosquitto-log swr_orange-replay-conf-swr
+  # et ensuite le up -d...
+
   systemd.services."docker-compose-v2x-edge-node" = {
     description = "Run Docker for v2x edge node";
     wantedBy = [ "multi-user.target" ];
