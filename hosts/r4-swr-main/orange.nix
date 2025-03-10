f4502e2dbc5d6d0df33055a97600a40a6c65cccd
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-10 10:46:09 +0100

    FIX

--- hosts/r4-swr-main/orange.nix
+++ hosts/r4-swr-main/orange.nix
@@ -15,12 +15,13 @@
     description = "Run Docker for v2x edge node";
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
+      User = "me";
       EnvironmentFile = [
         "/home/me/4757-R4-SWR/orange-storage/DOTenv"
         "/home/me/4757-R4-SWR/orange-storage/credentials"
       ];
       ExecStartPre = "${pkgs.bash}/bin/bash -c '${pkgs.coreutils}/bin/echo $DOCK_PASS | ${pkgs.docker}/bin/docker login -u $DOCK_USER --password-stdin $DOCK_REMOTE'";
-      ExecStart = "${pkgs.docker-compose}/bin/docker-compose --project-name $DOCK_PROJECT -f /home/me/4757-R4-SWR/orange-storage/docker-compose-v2x-edge-node.yml up";
+      ExecStart = "${pkgs.docker-compose}/bin/docker-compose --project-name $DOCK_PROJECT -f /home/me/4757-R4-SWR/orange-storage/docker-compose-v2x-edge-node.yml up --remove-orphans";
       ExecStartPost = "${pkgs.docker}/bin/docker logout";
       Restart = "always";
       RestartSec = "5s";
@@ -31,12 +32,13 @@
     description = "Run Docker for allog";
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
+      User = "me";
       EnvironmentFile = [
         "/home/me/4757-R4-SWR/orange-storage/DOTenv"
         "/home/me/4757-R4-SWR/orange-storage/credentials"
       ];
       ExecStartPre = "${pkgs.bash}/bin/bash -c '${pkgs.coreutils}/bin/echo $DOCK_PASS | ${pkgs.docker}/bin/docker login -u $DOCK_USER --password-stdin $DOCK_REMOTE'";
-      ExecStart = "${pkgs.docker-compose}/bin/docker-compose --project-name $DOCK_PROJECT -f /home/me/4757-R4-SWR/orange-storage/docker-compose-allog.yml up";
+      ExecStart = "${pkgs.docker-compose}/bin/docker-compose --project-name $DOCK_PROJECT -f /home/me/4757-R4-SWR/orange-storage/docker-compose-allog.yml up --remove-orphans";
       ExecStartPost = "${pkgs.docker}/bin/docker logout";
       Restart = "always";
       RestartSec = "5s";
