1e3ae87a89bccc889bceed8396f0e518689b601c
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-11 18:02:54 +0100

    FIX

--- hosts/r4-swr-trunk/ds.nix
+++ hosts/r4-swr-trunk/ds.nix
@@ -17,22 +17,22 @@
     };
   };
 
-  systemd.services."auto-web-2" = {
-    description = "Run Firefox with a specific URL";
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      User = "me";
-      ExecStart = "${pkgs.firefox}/bin/firefox http://localhost:3000/right";
-      Restart = "always";
-      RestartSec = "5s";
-      Environment = [
-        "WAYLAND_DISPLAY=wayland-1"
-        "XDG_RUNTIME_DIR=/run/user/1000"
-        "MOZ_ENABLE_WAYLAND=1"
-        "GDK_BACKEND=wayland"
-      ];
-    };
-  };
+  # systemd.services."auto-web-2" = {
+  #   description = "Run Firefox with a specific URL";
+  #   wantedBy = [ "multi-user.target" ];
+  #   serviceConfig = {
+  #     User = "me";
+  #     ExecStart = "${pkgs.firefox}/bin/firefox http://localhost:3000/right";
+  #     Restart = "always";
+  #     RestartSec = "5s";
+  #     Environment = [
+  #       "WAYLAND_DISPLAY=wayland-1"
+  #       "XDG_RUNTIME_DIR=/run/user/1000"
+  #       "MOZ_ENABLE_WAYLAND=1"
+  #       "GDK_BACKEND=wayland"
+  #     ];
+  #   };
+  # };
 
   systemd.services."ds-client" = {
     description = "Run React Client";
