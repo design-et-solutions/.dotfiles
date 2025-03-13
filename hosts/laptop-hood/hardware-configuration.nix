8c493884729b8cbb1f4d83d5a2111ae3a9987fdc
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-13 21:40:47 +0100

    FIX

--- hosts/laptop-hood/hardware-configuration.nix
+++ hosts/laptop-hood/hardware-configuration.nix
@@ -14,19 +14,79 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/ce471643-cd96-4e57-9fc3-cdd08f341fa3";
-    fsType = "ext4";
+  disko.devices = {
+    disk.disk1 = {
+      device = "/dev/nvme0n1";
+      type = "disk";
+      content = {
+        type = "gpt"; # Specifies that the partition table type is GPT (GUID Partition Table).
+        partitions = {
+          boot = {
+            name = "boot";
+            size = "1M";
+            type = "EF02"; # The partition type, which is typically used for BIOS boot partitions.
+          };
+          # The partition, which stands for EFI System Partition (ESP).
+          esp = {
+            name = "ESP";
+            size = "500M";
+            type = "EF00"; # The partition type for EFI System Partitions.
+            content = {
+              type = "filesystem";
+              format = "vfat"; # The filesystem format, which is VFAT (Virtual File Allocation Table).
+              mountpoint = "/boot/efi";
+            };
+          };
+          root = {
+            size = "100%";
+            content = {
+              type = "lvm_pv"; # Specifies that this partition is a physical volume for LVM (Logical Volume Manager).
+              vg = "pool"; # The volume group to which this physical volume belongs, named pool.
+            };
+          };
+        };
+      };
+    };
+    # The root partition is part of an LVM volume group, allowing for easy resizing and management of logical volumes.
+    lvm_vg = {
+      pool = {
+        type = "lvm_vg"; # Specifies that this is a volume group for LVM.
+        lvs = {
+          root = {
+            size = "100%FREE - 4G"; # The size of the logical volume, which takes up all the free space in the volume group.
+            content = {
+              type = "filesystem";
+              format = "ext4"; # The filesystem format, which is ext4, a commonly used filesystem for Linux.
+              mountpoint = "/"; # The mount point for this logical volume, which is the root filesystem (/).
+              mountOptions = [
+                "defaults"
+              ];
+            };
+          };
+          swap = {
+            size = "4G"; # Size of the swap logical volume
+            content = {
+              type = "swap";
+            };
+          };
+        };
+      };
+    };
   };
 
-  fileSystems."/boot/efi" = {
-    device = "/dev/disk/by-uuid/6D99-9B67";
-    fsType = "vfat";
-    options = [
-      "fmask=0022"
-      "dmask=0022"
-    ];
-  };
+  # fileSystems."/" = {
+  #   device = "/dev/disk/by-uuid/ce471643-cd96-4e57-9fc3-cdd08f341fa3";
+  #   fsType = "ext4";
+  # };
+  #
+  # fileSystems."/boot/efi" = {
+  #   device = "/dev/disk/by-uuid/6D99-9B67";
+  #   fsType = "vfat";
+  #   options = [
+  #     "fmask=0022"
+  #     "dmask=0022"
+  #   ];
+  # };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
--- nixos/core/system/ssh.nix
+++ nixos/core/system/ssh.nix
@@ -65,102 +65,10 @@
       "~@cpu-emulation"
       "~@debug"
     ];
-
     SystemCallArchitectures = "native";
-
-    # CapabilityBoundingSet = [
-    #   "CAP_SYS_ADMIN"
-    #   "CAP_SETUID"
-    #   "CAP_SETGID"
-    #   "CAP_SETPCAP"
-    #   "CAP_KILL"
-    #   "CAP_SYS_TTY_CONFIG"
-    #   "CAP_DAC_OVERRIDE"
-    #   "CAP_DAC_READ_SEARCH"
-    #   "CAP_FOWNER"
-    #   "CAP_IPC_OWNER"
-    #   "CAP_FSETID"
-    #   "CAP_SETFCAP"
-    #   "CAP_CHOWN"
-    # ];
-    # AmbientCapabilities = [ ];
 
     MemoryDenyWriteExecute = true;
     LockPersonality = true;
     DevicePolicy = "closed";
   };
-
-  # NoNewPrivileges = false;
-  #
-  # ProtectSystem = "full";
-  # ProtectControlGroups = true;
-  # ProtectClock = true;
-  # ProtectKernelModules = true;
-  # ProtectProc = "default";
-  #
-  # PrivateMounts = true;
-  # PrivateIPC = true;
-  #
-  # RestrictSUIDSGID = true;
-  # RestrictRealtime = true;
-  # RestrictFileSystems = [ ];
-  # RestrictNamespaces = [
-  #   "~cgroup"
-  #   "uts"
-  #   "pid"
-  #   "net"
-  #   "user"
-  #   "mnt"
-  #   "ipc"
-  # ];
-  # RestrictAddressFamilies = [
-  #   "AF_UNIX"
-  #   "AF_NETLINK"
-  #   "AF_INET"
-  #   "AF_INET6"
-  #   "~AF_PACKET"
-  # ];
-  #
-  # SystemCallArchitectures = [ "native" ];
-  # SystemCallErrorNumber = "EPERM";
-  # SystemCallFilter = [
-  #   "~@obsolete"
-  #   "~@cpu-emulation"
-  #   "~@clock"
-  #   "~@swap"
-  #   "~@module"
-  #   "~@reboot"
-  #   "~@raw-io"
-  #   "~@debug"
-  # ];
-  #
-  # CapabilityBoundingSet = [
-  #   "CAP_SYS_ADMIN"
-  #   "CAP_SETUID"
-  #   "CAP_SETGID"
-  #   "CAP_SETPCAP"
-  #   "CAP_KILL"
-  #   "CAP_SYS_TTY_CONFIG"
-  #   "CAP_DAC_OVERRIDE"
-  #   "CAP_DAC_READ_SEARCH"
-  #   "CAP_FOWNER"
-  #   "CAP_IPC_OWNER"
-  #   "CAP_FSETID"
-  #   "CAP_SETFCAP"
-  #   "CAP_CHOWN"
-  # ];
-  # AmbientCapabilities = [ ];
-  #
-  # DevicePolicy = "closed";
-  # DeviceAllow = [
-  #   "/dev/tty7 rw"
-  #   "/dev/input/* rw" # Allow Wayland to access keyboards/mice
-  #   "/dev/dri/* rw" # Allow access to GPU devices
-  # ];
-  #
-  # LockPersonality = true;
-  # UMask = "077";
-  # IPAddressDeny = "any";
-  # KeyringMode = lib.mkForce "private";
-
 }
