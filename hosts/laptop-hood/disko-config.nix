7477314a919d7d9222b7f327b7e9122d8da79794
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-13 22:19:23 +0100

    SDFIlk

--- hosts/laptop-hood/disko-config.nix
+++ hosts/laptop-hood/disko-config.nix
@@ -8,7 +8,7 @@
   disko.devices = {
     disk = {
       main = {
-        device = "/dev/nvme0n1"; # Adjust this to match your actual device
+        device = "/dev/nvme0"; # Adjust this to match your actual device
         type = "disk";
         content = {
           type = "gpt";
