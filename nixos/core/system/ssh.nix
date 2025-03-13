86383cc1139a5196cb4aff43060a3a2a9518434c
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-13 21:27:35 +0100

    sdklf

--- nixos/core/system/ssh.nix
+++ nixos/core/system/ssh.nix
@@ -48,14 +48,13 @@
     RestrictNamespaces = true;
     RestrictRealtime = true;
     RestrictSUIDSGID = true;
-    # RestrictAddressFamilies = false;
-    # RestrictAddressFamilies = [
-    #   "~AF_UNIX"
-    #   "~AF_NETLINK"
-    #   "AF_INET"
-    #   "AF_INET6"
-    #   "~AF_PACKET"
-    # ];
+    RestrictAddressFamilies = [
+      "AF_UNIX"
+      "~AF_NETLINK"
+      "AF_INET"
+      "AF_INET6"
+      "~AF_PACKET"
+    ];
 
     SystemCallFilter = [
       "~@keyring"
@@ -69,21 +68,21 @@
 
     SystemCallArchitectures = "native";
 
-    CapabilityBoundingSet = [
-      "CAP_SYS_ADMIN"
-      "CAP_SETUID"
-      "CAP_SETGID"
-      "CAP_SETPCAP"
-      "CAP_KILL"
-      "CAP_SYS_TTY_CONFIG"
-      "CAP_DAC_OVERRIDE"
-      "CAP_DAC_READ_SEARCH"
-      "CAP_FOWNER"
-      "CAP_IPC_OWNER"
-      "CAP_FSETID"
-      "CAP_SETFCAP"
-      "CAP_CHOWN"
-    ];
+    # CapabilityBoundingSet = [
+    #   "CAP_SYS_ADMIN"
+    #   "CAP_SETUID"
+    #   "CAP_SETGID"
+    #   "CAP_SETPCAP"
+    #   "CAP_KILL"
+    #   "CAP_SYS_TTY_CONFIG"
+    #   "CAP_DAC_OVERRIDE"
+    #   "CAP_DAC_READ_SEARCH"
+    #   "CAP_FOWNER"
+    #   "CAP_IPC_OWNER"
+    #   "CAP_FSETID"
+    #   "CAP_SETFCAP"
+    #   "CAP_CHOWN"
+    # ];
     AmbientCapabilities = [ ];
 
     MemoryDenyWriteExecute = true;
