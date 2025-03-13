031805c5839c1b9506144fac2935a72d0527145c
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-13 20:17:24 +0100

    FIX

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,13 +1,13 @@
 # hosts/default.nix
-{ mkNixosConfiguration, nixos-hardware, lib, ... }:
+{ mkNixosConfiguration, lib, ... }:
 
 let
   # Get all subdirectories in the current directory
   hostDirs = lib.filterAttrs (n: v: v == "directory") (builtins.readDir ./.);
 
   # Import each host.nix file and create the configuration
-  hostConfigurations = lib.mapAttrs (name: _: 
-    import (./. + "/${name}/host.nix") { inherit mkNixosConfiguration nixos-hardware; }
+  hostConfigurations = lib.mapAttrs (
+    name: _: import (./. + "/${name}/host.nix") { inherit mkNixosConfiguration; }
   ) hostDirs;
 in
-  hostConfigurations
+hostConfigurations
