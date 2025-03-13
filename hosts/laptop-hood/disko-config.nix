1c5023054371c3e38b4ec1a7e2e65c9d929146ec
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-13 22:22:58 +0100

    jjqFSLDKJF

--- hosts/laptop-hood/disko-config.nix
+++ hosts/laptop-hood/disko-config.nix
@@ -5,34 +5,34 @@
   ...
 }:
 {
-  disko.devices = {
-    disk = {
-      main = {
-        device = "/dev/nvme0"; # Adjust this to match your actual device
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            boot = {
-              size = "512M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot/efi";
-              };
-            };
-            root = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
-            };
-          };
-        };
-      };
-    };
-  };
+  # disko.devices = {
+  #   disk = {
+  #     main = {
+  #       device = "/dev/nvme0"; # Adjust this to match your actual device
+  #       type = "disk";
+  #       content = {
+  #         type = "gpt";
+  #         partitions = {
+  #           boot = {
+  #             size = "512M";
+  #             type = "EF00";
+  #             content = {
+  #               type = "filesystem";
+  #               format = "vfat";
+  #               mountpoint = "/boot/efi";
+  #             };
+  #           };
+  #           root = {
+  #             size = "100%";
+  #             content = {
+  #               type = "filesystem";
+  #               format = "ext4";
+  #               mountpoint = "/";
+  #             };
+  #           };
+  #         };
+  #       };
+  #     };
+  #   };
+  # };
 }
--- nixos/core/config.nix
+++ nixos/core/config.nix
@@ -5,6 +5,6 @@
     kexec-tools # Utilities for Linux that allows you to load and boot into a new kernel from the currently running kernel.
     grub2 # Boot loader for BIOS and UEFI systems; includes grub-mkpasswd-pbkdf2 for password generation
     efibootmgr # Tool to manipulate UEFI boot managers
-    # disko # Declarative disk partitioning and formatting using nix.
+    disko # Declarative disk partitioning and formatting using nix.
   ];
 }
