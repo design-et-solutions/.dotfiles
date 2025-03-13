3caa9603704fc3e1a57b5bf918c03048dc3a3b5b
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-14 00:01:37 +0100

    TEST

--- nixos/users/root.nix
+++ nixos/users/root.nix
@@ -9,7 +9,7 @@
   password = "root";
   # hashedPassword = "$6$9kBLqUdAM7HL/woP$7M.oa2WdVlOP6PyMNpKHX2Mn5fBe49Z6y/uVi2B9x/G2hjg5uYHhGb354KA97Uz/9pOVTNcrZ4tqatWITR82o1";
 
-  security.sudo.wheelNeedsPassword = false;
+  # security.sudo.wheelNeedsPassword = false;
 
   openssh.authorizedKeys.keys = mergedSetup.networking.internet.ssh.root.authorizedKeys;
 }
