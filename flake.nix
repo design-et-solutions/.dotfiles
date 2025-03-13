442a28aeaaedbc584bcb54ea7bad5aac7ecb0bb5
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-14 00:21:41 +0100

    sdlf

--- flake.nix
+++ flake.nix
@@ -122,14 +122,17 @@
                 );
               }
 
-              # inputs.nixos-facter-modules.nixosModules.facter
-              # {
-              #   config.facter.reportPath =
-              #     if builtins.pathExists ./facter.json then
-              #       ./facter.json
-              #     else
-              #       throw "Have you forgotten to run nixos-anywhere with `--generate-hardware-config nixos-facter ./facter.json`?";
-              # }
+              inputs.nixos-facter-modules.nixosModules.facter
+              {
+                config.facter.reportPath =
+                  if builtins.pathExists ./facter.json then
+                    ./facter.json
+                  else
+                    throw ''
+                      Have you forgotten to run nixos-anywhere with `--generate-hardware-config nixos-facter ./facter.json`? Or
+                      Have you forgotten to  build ./facter.json`?\n
+                    '';
+              }
             ]
             ++ extraModules
             ++ debugModules
