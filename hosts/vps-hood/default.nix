2360dc8b9a4f5eb4b4d3b7f4576ec02c5e7a014d
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-14 01:29:19 +0100

    TRY

--- hosts/vps-hood/default.nix
+++ hosts/vps-hood/default.nix
@@ -14,4 +14,8 @@
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMc6jbhoDuKt0YOIF9prT4reT9WG6sP2sEFVj59loQwq me@desktop-hood"
   ];
+
+  boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.grub.efiSupport = false;
+  # boot.loader.grub.device = "dev/sda";
 }
