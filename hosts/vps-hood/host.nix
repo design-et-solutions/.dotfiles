54b73656b898a21b51f4a039d166832c148308bf
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-14 01:00:54 +0100

    TRY

--- hosts/vps-hood/host.nix
+++ hosts/vps-hood/host.nix
@@ -6,7 +6,12 @@ mkNixosConfiguration {
   host = ./.;
   users = [ "me" ];
   setup = {
-    networking.internet.analyzer.enable = true;
+    networking.internet = {
+      ssh.root.authorizedKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMc6jbhoDuKt0YOIF9prT4reT9WG6sP2sEFVj59loQwq me@desktop-hood"
+      ];
+      analyzer.enable = true;
+    };
     security = {
       blocker.enable = true;
       analyzer.enable = true;
