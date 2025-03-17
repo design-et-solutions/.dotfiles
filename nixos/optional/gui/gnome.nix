65a4d1170b51aafffe154efbb0c3774f949f9702
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-17 19:05:45 +0100

    FIX

--- nixos/optional/gui/gnome.nix
+++ nixos/optional/gui/gnome.nix
@@ -1,16 +1,38 @@
 {
   pkgs,
+  mergedSetup,
+  lib,
   ...
 }:
 let
   isWayland = mergedSetup.gui.params.displayServer == "wayland";
 in
 {
-  environment.systemPackages = lib.mkIf (!isWayland) (
-    with pkgs;
-    [
-      gnome.gnome-shell
-      mozjs
-    ]
-  );
+  programs.dconf = lib.mkIf isWayland {
+    enable = true;
+  };
+
+  environment.systemPackages =
+    lib.mkIf (!isWayland) (
+      with pkgs;
+      [
+        gnome-photos
+        gnome-tour
+      ]
+    )
+    ++ (with pkgs.gnome; [
+      cheese
+      gnome-music
+      gedit
+      epiphany
+      geary
+      gnome-characters
+      tali
+      iagno
+      hitori
+      atomix
+      yelp
+      gnome-contacts
+      gnome-initial-setup
+    ]);
 }
