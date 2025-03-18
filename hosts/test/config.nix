da5d433186c4184b4fb29446b42878bc41ecdfb8
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-18 18:18:22 +0100

    FIX

--- hosts/test/config.nix
+++ hosts/test/config.nix
@@ -4,15 +4,5 @@ mkNixosConfiguration {
   name = "test";
   system = "x86_64-linux";
   users = [ "me" ];
-  hostConfig = {
-    gui.enable = true;
-    browser.firefox.enable = true;
-    networking.internet = {
-      analyzer.enable = true;
-    };
-    security = {
-      blocker.enable = true;
-      analyzer.enable = true;
-    };
-  };
+  hostConfig = { };
 }
--- hosts/test/default.nix
+++ hosts/test/default.nix
@@ -6,7 +6,6 @@
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
     (modulesPath + "/profiles/qemu-guest.nix")
-    ../disk-config.nix
   ];
 
   users.users.root.openssh.authorizedKeys.keys = [
@@ -14,12 +13,92 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAq7LsVEV+jw4yPpLyDc4XIS2yVmSJt0J24pS4BQYtGD me@laptop-work"
   ];
 
-  disko.devices.disk.disk1.device = "/dev/nvme0n1";
-
-  services = {
-    displayManager = {
-      autoLogin.enable = true;
-      autoLogin.user = "me";
+  disko.devices = {
+    disk.disk1 = {
+      device = "/dev/nvme0n1";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          boot = {
+            name = "boot";
+            size = "2M";
+            type = "EF02";
+          };
+          esp = {
+            name = "ESP";
+            size = "1G";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [
+                "fmask=0077"
+                "dmask=0077"
+              ];
+            };
+          };
+          root = {
+            name = "root";
+            size = "200G";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              mountOptions = [
+                "defaults"
+                "noatime"
+              ];
+            };
+          };
+          home = {
+            name = "home";
+            size = "100G";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/home";
+              mountOptions = [
+                "defaults"
+                "noatime"
+              ];
+            };
+          };
+          var = {
+            name = "var";
+            size = "100G";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/var";
+              mountOptions = [
+                "defaults"
+                "noatime"
+                "nodev"
+                "nosuid"
+              ];
+            };
+          };
+          swap = {
+            name = "swap";
+            size = "16G";
+            content = {
+              type = "swap";
+              randomEncryption = true;
+            };
+          };
+        };
+      };
     };
   };
+
+  # boot.consoleLogLevel = 7;
+
+  # services = {
+  #   displayManager = {
+  #     autoLogin.enable = true;
+  #     autoLogin.user = "me";
+  #   };
+  # };
 }
