ddb7cdbeece091b81d9b5eaef64936bddea325e0
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-18 18:18:32 +0100

    FIX

---
+++ hosts/test-2/config.nix
@@ -0,0 +1,31 @@
+{ mkNixosConfiguration, ... }:
+
+mkNixosConfiguration {
+  name = "test";
+  system = "x86_64-linux";
+  users = [ "me" ];
+  hostConfig = {
+    gui = {
+      enable = true;
+      params = {
+        displayServer = {
+          wayland = false;
+          x11 = true;
+        };
+        windowManager = {
+          hyprland = false;
+          i3 = true;
+        };
+      };
+    };
+    # gpu.nvidia.enable = true;
+    # browser.firefox.enable = true;
+    # networking.internet = {
+    # analyzer.enable = true;
+    # };
+    # security = {
+    # blocker.enable = true;
+    # analyzer.enable = true;
+    # };
+  };
+}
