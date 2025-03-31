741fa9654b1798587d016e7bf5fec4117cf3338b
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-03-31 13:35:55 +0200

    FIX

--- hosts/r4-swr-main/orange.nix
+++ hosts/r4-swr-main/orange.nix
@@ -32,20 +32,20 @@
     };
   };
 
-  systemd.services."docker-compose-allog" = {
-    description = "Run Docker for allog";
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      User = "me";
-      EnvironmentFile = [
-        "/home/me/4757-R4-SWR/orange-storage/DOTenv"
-        "/home/me/4757-R4-SWR/orange-storage/credentials"
-      ];
-      ExecStartPre = "${pkgs.bash}/bin/bash -c '${pkgs.coreutils}/bin/echo $DOCK_PASS | ${pkgs.docker}/bin/docker login -u $DOCK_USER --password-stdin $DOCK_REMOTE'";
-      ExecStart = "${pkgs.docker-compose}/bin/docker-compose --project-name $DOCK_PROJECT -f /home/me/4757-R4-SWR/orange-storage/docker-compose-allog.yml up --remove-orphans";
-      ExecStartPost = "${pkgs.docker}/bin/docker logout";
-      Restart = "always";
-      RestartSec = "5s";
-    };
-  };
+  # systemd.services."docker-compose-allog" = {
+  #   description = "Run Docker for allog";
+  #   wantedBy = [ "multi-user.target" ];
+  #   serviceConfig = {
+  #     User = "me";
+  #     EnvironmentFile = [
+  #       "/home/me/4757-R4-SWR/orange-storage/DOTenv"
+  #       "/home/me/4757-R4-SWR/orange-storage/credentials"
+  #     ];
+  #     ExecStartPre = "${pkgs.bash}/bin/bash -c '${pkgs.coreutils}/bin/echo $DOCK_PASS | ${pkgs.docker}/bin/docker login -u $DOCK_USER --password-stdin $DOCK_REMOTE'";
+  #     ExecStart = "${pkgs.docker-compose}/bin/docker-compose --project-name $DOCK_PROJECT -f /home/me/4757-R4-SWR/orange-storage/docker-compose-allog.yml up --remove-orphans";
+  #     ExecStartPost = "${pkgs.docker}/bin/docker logout";
+  #     Restart = "always";
+  #     RestartSec = "5s";
+  #   };
+  # };
 }
