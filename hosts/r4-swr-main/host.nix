0dbd3d807838c3038bcbcb9ec66db48c09e4c233
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-04-02 18:22:58 +0200

    FIX

--- hosts/r4-swr-main/host.nix
+++ hosts/r4-swr-main/host.nix
@@ -27,6 +27,7 @@ mkNixosConfiguration {
       };
       params.allowedPorts.tcp = [
         3000 # Server
+        4000 # Server Bis
         3001 # React
         9999 # WebRTC
         8554 # RTSP
