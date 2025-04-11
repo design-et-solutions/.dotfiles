8bfded86352a599b15058af3ad193fc25a9c66bd
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-04-11 12:23:23 +0200

    FIX

--- hosts/r4-swr-trunk/default.nix
+++ hosts/r4-swr-trunk/default.nix
@@ -58,6 +58,10 @@
     };
   };
 
+  environment.variables = {
+    MOZ_USE_XINPUT2 = "1";
+  };
+
   services.xserver.windowManager.i3.extraSessionCommands = ''
     # Disable screensaver
     xset s off
@@ -88,6 +92,7 @@
           # Assign Firefox instances to specific workspaces
           assign [class="firefox-1"] 1
           assign [class="firefox-2"] 2
+          assign [class="SightCohoma"] 1
 
           # Set Firefox instances to fullscreen on startup
           for_window [class="firefox-1"] fullscreen enable
@@ -99,25 +104,153 @@
         '';
       };
 
+      home.file."start_sight_app.sh" = {
+        text = ''
+          export DISPLAY=:0
+          xhost +local:docker
+          docker stop sight-container
+          docker container rm sight-container
+          docker run -d -it \
+                  --network host \
+                  -e DISPLAY=$DISPLAY \
+                  -v /tmp/.X11-unix:/tmp/.X11-unix \
+                  --device /dev/dri:/dev/dri \
+                  --name sight-container sight-image
+        '';
+        executable = true;
+      };
+
+      home.file."stop_sight_app.sh" = {
+        text = ''
+          export DISPLAY=:0
+          xhost +local:docker
+          docker stop sight-container
+          docker container rm sight-container
+        '';
+        executable = true;
+      };
+
+      home.file."change_tabs.sh" = {
+        text = ''
+          export DISPLAY=:0
+          #!/bin/sh
+
+          # Check if a tab number argument is provided
+          if [ -z "$1" ]; then
+            echo "Usage: $0 <tab_number>"
+            exit 1
+          fi
+
+          TAB_NUMBER=$1
+
+          # Get the window ID of the Firefox instance
+          WINDOW_ID=$(wmctrl -lx | grep 'firefox-1' | awk '{print $1}')
+
+          # Activate the window
+          wmctrl -ia $WINDOW_ID
+
+          # Send the key press to change tabs (Ctrl+$TAB_NUMBER)
+          xdotool key --window $WINDOW_ID ctrl+$TAB_NUMBER
+        '';
+        executable = true;
+      };
+
+      # Write touchegg.conf to the right place
+      home.file.".config/touchegg/touchegg.conf".text = ''
+        <touchégg>
+          <settings>
+            <property name="composed_gestures_time">111</property>
+          </settings>
+          <application name="All">
+            <gesture type="DRAG" fingers="1" direction="ALL">
+              <action type="DRAG_AND_DROP">BUTTON=1</action>
+            </gesture>
+            <gesture type="DRAG" fingers="4" direction="DOWN">
+              <action type="SEND_KEYS">Super+a</action>
+            </gesture>
+            <gesture type="DRAG" fingers="4" direction="UP">
+              <action type="SEND_KEYS">Super+s</action>
+            </gesture>
+            <gesture type="DRAG" fingers="4" direction="RIGHT">
+              <action type="SEND_KEYS">Super+Left</action>
+            </gesture>
+            <gesture type="DRAG" fingers="4" direction="LEFT">
+              <action type="SEND_KEYS">Super+Right</action>
+            </gesture>
+            <gesture type="DRAG" fingers="3" direction="UP">
+              <action type="MAXIMIZE_RESTORE_WINDOW"></action>
+            </gesture>
+            <gesture type="DRAG" fingers="3" direction="DOWN">
+              <action type="MINIMIZE_WINDOW"></action>
+            </gesture>
+            <gesture type="DRAG" fingers="3" direction="RIGHT">
+              <action type="SEND_KEYS">Control+Super+Right</action>
+            </gesture>
+            <gesture type="DRAG" fingers="3" direction="LEFT">
+              <action type="SEND_KEYS">Control+Super+Left</action>
+            </gesture>
+            <gesture type="DRAG" fingers="2" direction="ALL">
+              <action type="SCROLL">SPEED=7:INVERTED=true</action>
+            </gesture>
+
+            <gesture type="PINCH" fingers="2" direction="IN">
+              <action type="SEND_KEYS">
+                <modifiers>Control_L</modifiers>
+                <keys>minus</keys>
+                <on>begin</on>
+              </action>
+            </gesture>
+
+            <gesture type="PINCH" fingers="2" direction="OUT">
+              <action type="SEND_KEYS">
+                <modifiers>Control_L</modifiers>
+                <keys>equal</keys>
+                <on>begin</on>
+              </action>
+            </gesture>
+
+            <gesture type="TAP" fingers="3" direction="">
+              <action type="MOUSE_CLICK">BUTTON=2</action>
+            </gesture>
+            <gesture type="TAP" fingers="2" direction="">
+              <action type="MOUSE_CLICK">BUTTON=3</action>
+            </gesture>
+            <gesture type="TAP" fingers="1" direction="">
+              <action type="MOUSE_CLICK">BUTTON=1</action>
+            </gesture>
+          </application>
+          <application name="Gwenview, Shotwell, Evince">
+            <gesture type="ROTATE" fingers="2" direction="LEFT">
+              <action type="SEND_KEYS">Control+L</action>
+            </gesture>
+            <gesture type="PINCH" fingers="2" direction="IN">
+              <action type="SEND_KEYS">Control+KP_Add</action>
+            </gesture>
+            <gesture type="PINCH" fingers="2" direction="OUT">
+              <action type="SEND_KEYS">Control+KP_Subtract</action>
+            </gesture>
+            <gesture type="ROTATE" fingers="2" direction="RIGHT">
+              <action type="SEND_KEYS">Control+R</action>
+            </gesture>
+          </application>
+          <application name="Dolphin, Midori, Chromium-browser, Chrome, Firefox">
+            <gesture type="DRAG" fingers="5" direction="RIGHT">
+              <action type="SEND_KEYS">Alt+Home</action>
+            </gesture>
+            <gesture type="DRAG" fingers="5" direction="ALL">
+              <action type="SEND_KEYS">Control+Next</action>
+            </gesture>
+          </application>
+        </touchégg>
+      '';
+
+      xsession.windowManager.i3.config.startup = [
+        {
+          command = "touchegg";
+          always = true;
+        }
+      ];
     };
-
-  services.touchegg.enable = true;
-
-  #
-  # systemd.services."touch" = {
-  #   description = "Setup touch screens";
-  #   wantedBy = [ "multi-user.target" ];
-  #   serviceConfig = {
-  #     User = "me";
-  #     ExecStart = "/run/current-system/sw/bin/xinput map-to-output 10 HDMI-1 && /run/current-system/sw/bin/xinput map-to-output 11 HDMI-2";
-  #     Restart = "always";
-  #     RestartSec = "5s";
-  #     Environment = [
-  #       "DISPLAY=:0"
-  #       "XDG_RUNTIME_DIR=/run/user/1000"
-  #     ];
-  #   };
-  # };
 
   # systemd.services."thales-sight" = {
   #   description = "Run Thales App Sight";
@@ -139,23 +272,15 @@
     "net.ipv4.conf.all.force_igmp_version" = 2;
   };
 
-  environment.variables = {
-    # PKG_CONFIG_PATH = "${pkgs.gst_all_1.gstreamer.dev}/lib/pkgconfig:${pkgs.gst_all_1.gst-plugins-base.dev}/lib/pkgconfig";
-  };
-
-  # networking.routes = [
-  #   {
-  #     address = "224.0.0.0";
-  #     prefixLength = 3;
-  #     via = null;
-  #     interface = "enp86s0";
-  #   }
-  # ];
+  services.touchegg.enable = true;
 
   environment.systemPackages = with pkgs; [
     xdotool
-    # libinput-gestures
-    # unclutter
+    libinput-gestures
+    libinput
+    touchegg
+    wmctrl
+
     tcpdump
     natscli
 
