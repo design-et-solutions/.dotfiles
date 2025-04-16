e93bee3ab2220f201bb800d3e983e3fb2a264e2d
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-04-16 18:20:15 +0200

    FIX

--- hosts/r4-swr-trunk/default.nix
+++ hosts/r4-swr-trunk/default.nix
@@ -132,9 +132,12 @@
           for_window [class="SightCohoma"] fullscreen enable
           for_window [class="firefox-2"] fullscreen enable
 
-          bindsym q [title="SightCohoma"] focus; fullscreen enable
-          bindsym w [instance="firefox-1"] focus; fullscreen enable
-          bindsym e [instance="firefox-2"] focus; fullscreen enable
+          bindsym $mod+s [class="SightCohoma"] focus
+          bindsym $mod+1 [title="Create Next App — Mozilla Firefox" instance="firefox-1"] focus
+          bindsym $mod+2 [title="Create Next App — Mozilla Firefox" instance="firefox-2"] focus
+          # bindsym q [title="SightCohoma"] focus; fullscreen enable
+          # bindsym w [instance="firefox-1"] focus; fullscreen enable
+          # bindsym e [instance="firefox-2"] focus; fullscreen enable
         '';
       };
 
