b7d89786e50d41eaccdf39d3acc38f67d1d741ba
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-04-22 16:28:40 +0200

    wip

---
+++ hosts/r4-swr-simu/dron_simu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while true; do
+    if ! timeout 2 ffprobe -v error -rtsp_transport tcp rtsp://localhost:8554/vivatech-simu | grep -q "Stream"; then
+        echo "Simulation not responding, restarting..."
+        sphinx-cli action -m world fwman world_reset_all
+    fi
+    sleep 5
+done
