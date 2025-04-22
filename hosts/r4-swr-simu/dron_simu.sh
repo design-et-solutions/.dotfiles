a52ae68ca376fa1e02cc54affc2cbc13f07a0182
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-04-22 18:25:31 +0200

    wip

--- hosts/r4-swr-simu/dron_simu.sh
+++ hosts/r4-swr-simu/dron_simu.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
+THRESHOLD=10
+NO_MOVE_COUNT=0
+
+get_state() {
+    tlm-data-logger inet:127.0.0.1:9060 | \
+      grep -E 'relativeLinearVelocity|worldPosition' | \
+      grep -m6 -E 'x:|y:|z:' | \
+      awk '{print $2}' | tr '\n' ' ' | md5sum | awk '{print $1}'
+}
+
+LAST_STATE=$(get_state)
+
 while true; do
-    if ! timeout 2 ffprobe -v error -rtsp_transport tcp rtsp://localhost:8554/vivatech-simu | grep -q "Stream"; then
-        echo "Simulation not responding, restarting..."
-        sphinx-cli action -m world fwman world_reset_all
+    sleep 1
+    CURRENT_STATE=$(get_state)
+    if [[ "$CURRENT_STATE" == "$LAST_STATE" ]]; then
+        ((NO_MOVE_COUNT++))
+        echo "No movement detected: $NO_MOVE_COUNT seconds"
+        if (( NO_MOVE_COUNT >= THRESHOLD )); then
+            echo "Drone appears stuck! Restarting drone..."
+            sphinx-cli reset
+            NO_MOVE_COUNT=0
+        fi
+    else
+        echo "Mouvement detected: $CURRENT_STATE"
+        NO_MOVE_COUNT=0
+        LAST_STATE="$CURRENT_STATE"
     fi
-    sleep 20
 done
