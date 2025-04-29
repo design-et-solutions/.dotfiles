9297441620669bfe240860263bad15dc69158f02
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-04-29 16:43:08 +0200

    fmt

--- hosts/r4-swr-simu/firefox_1_open.sh
+++ hosts/r4-swr-simu/firefox_1_open.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export DISPLAY=:0
-xdotool search --class firefox-1 | tail -1 | xargs xdotool windowactivate
+wmctrl -xa Navigator.firefox-1
--- hosts/r4-swr-simu/firefox_2_open.sh
+++ hosts/r4-swr-simu/firefox_2_open.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export DISPLAY=:0
-xdotool search --class firefox-2 | tail -1 | xargs xdotool windowactivate
+wmctrl -xa Navigator.firefox-2
