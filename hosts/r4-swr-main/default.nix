b40b2e73af4615205677e4bebf27aadbf03963c4
wallago <45556867+wallago@users.noreply.github.com>
2026-02-25 13:29:41 +0100

    refactor(main): remove unsused services

--- hosts/r4-swr-main/default.nix
+++ hosts/r4-swr-main/default.nix
@@ -1,12 +1,13 @@
-{ pkgs, lib, ... }: {
+{ pkgs, lib, ... }:
+{
   imports = [
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
     ./ds.nix
-    ./eviden.nix
-    ./orange.nix
-    ./thales.nix
-    ./test.nix
+    # ./eviden.nix
+    # ./orange.nix
+    # ./thales.nix
+    # ./test.nix
   ];
 
   time.timeZone = "Europe/Paris";
@@ -17,6 +18,8 @@
 
   networking = {
     hostName = "r4-swr-main";
-    hosts = { "192.100.1.1" = [ "cdp.thales" ]; };
+    hosts = {
+      "192.1.1.1" = [ "cdp.thales" ];
+    };
   };
 }
